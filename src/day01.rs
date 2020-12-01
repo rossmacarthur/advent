f810988f9622471923d4fdb254c015583b84f91a
Ross MacArthur <ross@macarthur.io>
2020-12-01 18:19:46 +0200

    Implement better solutions for Day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,6 +1,10 @@
+use std::cmp::Ordering::*;
+
 use rand::Rng;
 
 const INPUT: &str = include_str!("day01.txt");
+
+const SUM: u32 = 2020;
 
 pub fn default_input() -> Vec<u32> {
     INPUT.lines().map(|line| line.parse().unwrap()).collect()
@@ -13,29 +17,45 @@ pub fn random_input(count: usize) -> Vec<u32> {
     input
 }
 
-pub fn solve_sum_two(numbers: &[u32]) -> Option<(u32, u32)> {
-    for i in 0..numbers.len() {
-        for j in i + 1..numbers.len() {
-            let a = numbers[i];
-            let b = numbers[j];
-            if a + b == 2020 {
-                return Some((a, b));
+pub fn solve_sum_two(mut numbers: Vec<u32>) -> Option<(u32, u32)> {
+    numbers.sort_unstable();
+    let mut left = 0;
+    let mut right = numbers.len() - 1;
+    while left < right {
+        let a = numbers[left];
+        let b = numbers[right];
+        let sum = a + b;
+        match sum.cmp(&SUM) {
+            Less => {
+                left += 1;
             }
+            Greater => {
+                right -= 1;
+            }
+            Equal => return Some((a, b)),
         }
     }
     None
 }
 
-pub fn solve_sum_three(numbers: &[u32]) -> Option<(u32, u32, u32)> {
-    for i in 0..numbers.len() {
-        for j in i + 1..numbers.len() {
-            for k in j + 1..numbers.len() {
-                let a = numbers[i];
-                let b = numbers[j];
-                let c = numbers[k];
-                if a + b + c == 2020 {
-                    return Some((a, b, c));
+pub fn solve_sum_three(mut numbers: Vec<u32>) -> Option<(u32, u32, u32)> {
+    numbers.sort_unstable();
+    for i in 0..numbers.len() - 2 {
+        let mut left = i + 1;
+        let mut right = numbers.len() - 1;
+        while left < right {
+            let a = numbers[left];
+            let b = numbers[right];
+            let c = numbers[i];
+            let sum = a + b + c;
+            match sum.cmp(&SUM) {
+                Less => {
+                    left += 1;
                 }
+                Greater => {
+                    right -= 1;
+                }
+                Equal => return Some((a, b, c)),
             }
         }
     }
