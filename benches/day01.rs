7f768ba0eded316aebab2a72d84bc5e0df7a6339
Ross MacArthur <ross@macarthur.io>
2020-12-01 18:19:46 +0200

    Implement better solutions for Day 1

--- benches/day01.rs
+++ benches/day01.rs
@@ -1,32 +1,48 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 
 use aoc::day01::*;
 
 fn bench_sum_two_default(c: &mut Criterion) {
     let input = default_input();
     c.bench_function("day01_sum_two_with_default", |b| {
-        b.iter(|| solve_sum_two(&input))
+        b.iter_batched(
+            || input.clone(),
+            |input| solve_sum_two(input),
+            BatchSize::SmallInput,
+        )
     });
 }
 
 fn bench_sum_three_default(c: &mut Criterion) {
     let input = default_input();
     c.bench_function("day01_sum_three_with_default", |b| {
-        b.iter(|| solve_sum_three(&input))
+        b.iter_batched(
+            || input.clone(),
+            |input| solve_sum_three(input),
+            BatchSize::SmallInput,
+        )
     });
 }
 
 fn bench_sum_two_random(c: &mut Criterion) {
     let input = random_input(1000);
     c.bench_function("day01_sum_two_with_random", |b| {
-        b.iter(|| solve_sum_two(&input))
+        b.iter_batched(
+            || input.clone(),
+            |input| solve_sum_two(input),
+            BatchSize::SmallInput,
+        )
     });
 }
 
 fn bench_sum_three_random(c: &mut Criterion) {
     let input = random_input(1000);
     c.bench_function("day01_sum_three_with_random", |b| {
-        b.iter(|| solve_sum_three(&input))
+        b.iter_batched(
+            || input.clone(),
+            |input| solve_sum_three(input),
+            BatchSize::SmallInput,
+        )
     });
 }
 
--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -4,12 +4,12 @@ fn main() {
     let input = default_input();
 
     println!("Part 1:");
-    let (a, b) = solve_sum_two(&input).unwrap();
+    let (a, b) = solve_sum_two(input.clone()).unwrap();
     println!("  The numbers are: {}, {}", a, b);
     println!("  Multiplied together: {}", a * b);
 
     println!("Part 2:");
-    let (a, b, c) = solve_sum_three(&input).unwrap();
+    let (a, b, c) = solve_sum_three(input).unwrap();
     println!("  The numbers are: {}, {}, {}", a, b, c);
     println!("  Multiplied together: {}", a * b * c);
 }
