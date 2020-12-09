d9b163df07d27acd777d9893a0f61ed9344fa38b
Ross MacArthur <ross@macarthur.io>
2020-12-09 14:05:48 +0200

    Restructure code into a Cargo workspace
    Use a separate crate per year.

---
+++ 2019/src/main.rs
@@ -0,0 +1,35 @@
+use std::process;
+
+use clap::{AppSettings, Clap};
+
+macro_rules! run {
+    ($path:path) => {{
+        use $path::*;
+        let input = default_input();
+        let result = part1(&input);
+        println!("Part 1: {:?}", result);
+        let result = part2(&input);
+        println!("Part 2: {:?}", result);
+    }};
+}
+
+#[derive(Debug, Clap)]
+#[clap(global_setting = AppSettings::DisableVersion)]
+struct Opt {
+    #[clap(name = "DAY")]
+    day: Option<u32>,
+}
+
+fn main() {
+    let Opt { day } = Opt::parse();
+
+    match day {
+        Some(1) => run!(advent::day01),
+        Some(2) => run!(advent::day02),
+        Some(3) | None => run!(advent::day03),
+        Some(d) => {
+            eprintln!("Error: unknown day `{}`", d);
+            process::exit(2);
+        }
+    }
+}
--- src/lib.rs
+++
@@ -1,2 +0,0 @@
-pub mod year2019;
-pub mod year2020;
