bf714b9bedc2fcbe0bb5021d74f3697a04b1d456
Ross MacArthur <ross@macarthur.io>
2020-12-09 08:04:05 +0200

    Add 2020 Day 9 solution

--- benches/year2020/main.rs
+++ benches/year2020/main.rs
@@ -1,9 +1,11 @@
 mod day01;
 mod day02;
 mod day05;
+mod day09;
 
 criterion::criterion_main!(
     crate::day01::benches,
     crate::day02::benches,
-    crate::day05::benches
+    crate::day05::benches,
+    crate::day09::benches
 );
