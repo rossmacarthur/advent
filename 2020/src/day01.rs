9d788277e0f60c81c1d92c0105da72bce8c8b548
Ross MacArthur <ross@macarthur.io>
2020-12-25 13:28:12 +0200

    Touch up 2020 Day 1 solution
    - Add tests.
    - Re-use code between part 1 and part 2.

--- 2020/src/day01.rs
+++ 2020/src/day01.rs
@@ -1,7 +1,5 @@
 use std::cmp::Ordering::*;
 
-use rand::Rng;
-
 const INPUT: &str = include_str!("input/day01.txt");
 
 const SUM: u64 = 2020;
@@ -10,48 +8,49 @@ pub fn default_input() -> Vec<u64> {
     INPUT.lines().map(str::parse).map(Result::unwrap).collect()
 }
 
-pub fn random_input(count: usize) -> Vec<u64> {
-    let mut rng = rand::thread_rng();
-    let mut input = vec![0; count];
-    rng.fill(input.as_mut_slice());
-    input
+fn solve(numbers: &[u64], mut left: usize, c: u64) -> Option<u64> {
+    let mut right = numbers.len() - 1;
+    while left < right {
+        let a = numbers[left];
+        let b = numbers[right];
+        let sum = a + b + c;
+        match sum.cmp(&SUM) {
+            Less => left += 1,
+            Greater => right -= 1,
+            Equal => return Some(a * b),
+        }
+    }
+    None
 }
 
 pub fn part1(input: &[u64]) -> u64 {
     let mut numbers = input.to_vec();
     numbers.sort_unstable();
-    let mut left = 0;
-    let mut right = numbers.len() - 1;
-    while left < right {
-        let a = numbers[left];
-        let b = numbers[right];
-        let sum = a + b;
-        match sum.cmp(&SUM) {
-            Less => left += 1,
-            Greater => right -= 1,
-            Equal => return a * b,
-        }
-    }
-    unreachable!()
+    solve(&numbers, 0, 0).unwrap()
 }
 
 pub fn part2(input: &[u64]) -> u64 {
     let mut numbers = input.to_vec();
     numbers.sort_unstable();
     for i in 0..numbers.len() - 2 {
-        let mut left = i + 1;
-        let mut right = numbers.len() - 1;
-        while left < right {
-            let a = numbers[left];
-            let b = numbers[right];
-            let c = numbers[i];
-            let sum = a + b + c;
-            match sum.cmp(&SUM) {
-                Less => left += 1,
-                Greater => right -= 1,
-                Equal => return a * b * c,
-            }
+        let c = numbers[i];
+        if let Some(a_x_b) = solve(&numbers, i + 1, c) {
+            return a_x_b * c;
         }
     }
     unreachable!()
+}
+
+#[test]
+fn ex1() {
+    let input = [1721, 979, 366, 299, 675, 1456];
+    assert_eq!(part1(&input), 514579);
+    assert_eq!(part2(&input), 241861950);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(&input), 567171);
+    assert_eq!(part2(&input), 212428694);
 }
