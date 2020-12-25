38258146e2c74b9806d283b295609d186c100d1d
Ross MacArthur <ross@macarthur.io>
2020-12-25 13:28:12 +0200

    Touch up 2020 Day 1 solution
    - Add tests.
    - Re-use code between part 1 and part 2.

--- 2020/benches/day01.rs
+++ 2020/benches/day01.rs
@@ -2,30 +2,14 @@ use criterion::{criterion_group, Criterion};
 
 use advent::day01::*;
 
-fn bench_part1_with_default(c: &mut Criterion) {
+fn bench_part1(c: &mut Criterion) {
     let input = default_input();
-    c.bench_function("day01_part1_with_default", |b| b.iter(|| part1(&input)));
+    c.bench_function("day01_part1", |b| b.iter(|| part1(&input)));
 }
 
-fn bench_part2_with_default(c: &mut Criterion) {
+fn bench_part2(c: &mut Criterion) {
     let input = default_input();
-    c.bench_function("day01_part2_with_default", |b| b.iter(|| part2(&input)));
+    c.bench_function("day01_part2", |b| b.iter(|| part2(&input)));
 }
 
-fn bench_part1_with_random(c: &mut Criterion) {
-    let input = random_input(1000);
-    c.bench_function("day01_part1_with_random", |b| b.iter(|| part1(&input)));
-}
-
-fn bench_part2_with_random(c: &mut Criterion) {
-    let input = random_input(1000);
-    c.bench_function("day01_part2_with_random", |b| b.iter(|| part2(&input)));
-}
-
-criterion_group!(
-    benches,
-    bench_part1_with_default,
-    bench_part2_with_default,
-    bench_part1_with_random,
-    bench_part2_with_random
-);
+criterion_group!(benches, bench_part1, bench_part2);
