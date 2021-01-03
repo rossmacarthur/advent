9da52f14aa80b8d0cc1262289524f31b8e26fe7c
Ross MacArthur <ross@macarthur.io>
2021-01-03 12:05:30 +0200

    Add tests for 2020 Day 6 solution

--- 2020/src/day06.rs
+++ 2020/src/day06.rs
@@ -4,11 +4,15 @@ use reduce::Reduce;
 
 const INPUT: &str = include_str!("input/day06.txt");
 
-pub fn default_input() -> Vec<Vec<HashSet<char>>> {
-    INPUT
+fn parse_input(input: &str) -> Vec<Vec<HashSet<char>>> {
+    input
         .split("\n\n")
         .map(|group| group.lines().map(|line| line.chars().collect()).collect())
         .collect()
+}
+
+pub fn default_input() -> Vec<Vec<HashSet<char>>> {
+    parse_input(INPUT)
 }
 
 pub fn part1(input: &[Vec<HashSet<char>>]) -> usize {
@@ -37,4 +41,34 @@ pub fn part2(input: &[Vec<HashSet<char>>]) -> usize {
                 .unwrap()
         })
         .sum()
+}
+
+#[test]
+fn example() {
+    let input = parse_input(
+        r#"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"#,
+    );
+    assert_eq!(part1(&input), 11);
+    assert_eq!(part2(&input), 6);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(&input), 6587);
+    assert_eq!(part2(&input), 3235);
 }
