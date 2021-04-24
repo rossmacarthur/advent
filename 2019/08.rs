624331b280db4483da51f63cd0f4a9763b670296
Ross MacArthur <ross@macarthur.io>
2021-04-24 18:53:49 +0200

    Add main function and tests for 2019 Day 8

--- 2019/08.rs
+++ 2019/08.rs
@@ -1,41 +1,57 @@
-const INPUT: &str = include_str!("input/08.txt");
-const WIDTH: usize = 25;
-const HEIGHT: usize = 6;
-const SIZE: usize = WIDTH * HEIGHT;
-
-pub fn default_input() -> Vec<u32> {
-    INPUT
+fn parse_input(input: &str, width: usize, height: usize) -> Image {
+    let data = input
         .trim()
         .chars()
         .map(|c| c.to_digit(10))
         .map(Option::unwrap)
-        .collect()
+        .collect();
+    Image {
+        width,
+        height,
+        data,
+    }
+}
+
+fn default_input() -> Image {
+    parse_input(include_str!("input/08.txt"), 25, 6)
+}
+
+struct Image {
+    width: usize,
+    height: usize,
+    data: Vec<u32>,
+}
+
+impl Image {
+    const fn size(&self) -> usize {
+        self.width * self.height
+    }
 }
 
 fn count(layer: &[u32], pixel: u32) -> usize {
     layer.iter().filter(|&&p| p == pixel).count()
 }
 
-pub fn part1(image: &[u32]) -> usize {
-    image
-        .chunks(SIZE)
+fn part1(img: &Image) -> usize {
+    img.data
+        .chunks(img.size())
         .map(|layer| (count(layer, 0), layer))
         .min()
         .map(|(_, layer)| count(layer, 1) * count(layer, 2))
         .unwrap()
 }
 
-pub fn part2(image: &[u32]) -> String {
-    (0..SIZE)
+fn part2(img: &Image) -> String {
+    (0..img.size())
         .map(|i| {
             (0..)
-                .map(|layer| image[layer * SIZE + i])
+                .map(|layer| img.data[layer * img.size() + i])
                 .find(|&layer| layer != 2)
                 .unwrap()
         })
         .enumerate()
         .fold(String::new(), |mut s, (i, pixel)| {
-            if i % WIDTH == 0 {
+            if i % img.width == 0 {
                 s.push('\n');
             }
             s.push_str(match pixel {
@@ -45,4 +61,24 @@ pub fn part2(image: &[u32]) -> String {
             });
             s
         })
+}
+
+fn main() {
+    let mut run = advent::start();
+    let input = run.time("Parse input", default_input());
+    run.result("Part 1", part1(&input));
+    run.result("Part 2", part2(&input));
+    run.finish();
+}
+
+#[test]
+fn example() {
+    let input = parse_input("123456789012", 3, 2);
+    assert_eq!(part1(&input), 1);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(&input), 1677);
 }
