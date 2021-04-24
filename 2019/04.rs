96e0657ff13d0bc76a81d9442fc05001eacde42b
Ross MacArthur <ross@macarthur.io>
2021-04-24 17:42:40 +0200

    Add main function and test for 2019 Day 4

--- 2019/04.rs
+++ 2019/04.rs
@@ -1,48 +1,62 @@
-use std::collections::VecDeque;
 use std::ops::Range;
 
 use itertools::Itertools;
 
-pub fn default_input() -> Range<u64> {
+type Digits = [i8; 8];
+
+fn default_input() -> Range<u64> {
     123257..647015
 }
 
-fn digits(mut num: u64) -> Vec<i8> {
-    let mut digits = Vec::new();
-    while num > 0 {
-        digits.push((num % 10) as i8);
+fn digits(mut num: u64) -> Digits {
+    let mut digits = [-1, 0, 0, 0, 0, 0, 0, -1];
+    for i in (1..7).rev() {
+        digits[i] = (num % 10) as i8;
         num /= 10;
     }
     digits
 }
 
-fn has_increasing_digits(digits: &[i8]) -> bool {
-    digits.iter().tuple_windows().all(|(x, y)| y >= x)
+fn has_increasing(digits: &Digits) -> bool {
+    digits[1..7].iter().tuple_windows().all(|(x, y)| x <= y)
 }
 
-pub fn part1(input: &Range<u64>) -> usize {
+fn has_two_adjacent(digits: &Digits) -> bool {
+    digits[1..7].iter().tuple_windows().any(|(x, y)| x == y)
+}
+
+fn has_two_adjacent_excl(digits: &Digits) -> bool {
+    digits
+        .iter()
+        .tuple_windows()
+        .any(|(w, x, y, z)| w != x && x == y && y != z)
+}
+
+fn part1(input: Range<u64>) -> usize {
     input
-        .clone()
-        .filter(|&num| {
-            let digits = digits(num);
-            has_increasing_digits(&digits) && digits.iter().tuple_windows().any(|(x, y)| x == y)
-        })
+        .map(digits)
+        .filter(|digits| has_increasing(&digits) && has_two_adjacent(&digits))
         .count()
 }
 
-pub fn part2(input: &Range<u64>) -> usize {
+fn part2(input: Range<u64>) -> usize {
     input
-        .clone()
-        .filter(|&num| {
-            let digits = digits(num);
-            let mut digits_pad: VecDeque<_> = digits.clone().into();
-            digits_pad.push_back(-1);
-            digits_pad.push_front(-1);
-            has_increasing_digits(&digits)
-                && digits_pad
-                    .iter()
-                    .tuple_windows()
-                    .any(|(w, x, y, z)| x == y && w != x && y != z)
-        })
+        .map(digits)
+        .filter(|digits| has_increasing(&digits) && has_two_adjacent_excl(&digits))
         .count()
+}
+
+fn main() {
+    let mut run = advent::start();
+    let input = default_input();
+    run.result("Part 1", part1(input.clone()));
+    run.result("Part 2", part2(input));
+    run.finish();
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(input.clone()), 2220);
+    assert_eq!(part2(input), 1515);
 }
