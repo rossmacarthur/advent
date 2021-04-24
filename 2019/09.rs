cd39c1260734b4c56a447b5805c9dc6754da30cf
Ross MacArthur <ross@macarthur.io>
2021-04-24 23:05:40 +0200

    Move intcode computer into module

--- 2019/09.rs
+++ 2019/09.rs
@@ -1,121 +1,18 @@
-pub mod intcode;
+mod computer;
+mod intcode;
 
-use std::cmp::max;
-
-use intcode::{cast, parse_program, State};
+use computer::{parse_program, Computer};
 
 fn default_input() -> Vec<i64> {
     parse_program(include_str!("input/09.txt"))
 }
 
-#[derive(Debug)]
-pub struct Computer {
-    mem: Vec<i64>,
-    ptr: usize,
-    relative_base: i64,
-}
-
-impl Computer {
-    pub fn new(program: Vec<i64>) -> Self {
-        Self {
-            mem: program,
-            ptr: 0,
-            relative_base: 0,
-        }
-    }
-
-    fn mem_get(&self, addr: usize) -> i64 {
-        self.mem.get(addr).copied().unwrap_or(0)
-    }
-
-    fn mem_get_mut(&mut self, addr: usize) -> &mut i64 {
-        self.mem.resize(max(self.mem.len(), addr + 1), 0);
-        &mut self.mem[addr]
-    }
-
-    fn param_ptr(&self, i: usize) -> usize {
-        let opcode = self.mem_get(self.ptr);
-        let ptr = self.ptr + i;
-        match opcode / (10i64.pow((1 + i) as u32)) % 10 {
-            0 => cast(self.mem_get(ptr)),
-            1 => ptr,
-            2 => cast(self.relative_base + self.mem_get(ptr)),
-            mode => panic!("unknown mode `{}`", mode),
-        }
-    }
-
-    fn param(&self, i: usize) -> i64 {
-        self.mem_get(self.param_ptr(i))
-    }
-
-    fn param_mut(&mut self, i: usize) -> &mut i64 {
-        self.mem_get_mut(self.param_ptr(i))
-    }
-
-    pub fn next(&mut self, input: i64) -> State {
-        let mut input = Some(input);
-        loop {
-            match self.mem_get(self.ptr) % 100 {
-                1 => {
-                    *self.param_mut(3) = self.param(1) + self.param(2);
-                    self.ptr += 4;
-                }
-                2 => {
-                    *self.param_mut(3) = self.param(1) * self.param(2);
-                    self.ptr += 4;
-                }
-                3 => {
-                    if let Some(input) = input.take() {
-                        *self.param_mut(1) = input;
-                        self.ptr += 2;
-                    } else {
-                        break State::Waiting;
-                    }
-                }
-                4 => {
-                    let output = self.param(1);
-                    self.ptr += 2;
-                    break State::Yielded(output);
-                }
-                5 => {
-                    if self.param(1) != 0 {
-                        self.ptr = cast(self.param(2));
-                    } else {
-                        self.ptr += 3;
-                    }
-                }
-                6 => {
-                    if self.param(1) == 0 {
-                        self.ptr = cast(self.param(2));
-                    } else {
-                        self.ptr += 3;
-                    }
-                }
-                7 => {
-                    *self.param_mut(3) = (self.param(1) < self.param(2)) as i64;
-                    self.ptr += 4;
-                }
-                8 => {
-                    *self.param_mut(3) = (self.param(1) == self.param(2)) as i64;
-                    self.ptr += 4;
-                }
-                9 => {
-                    self.relative_base += self.param(1);
-                    self.ptr += 2;
-                }
-                99 => break State::Complete,
-                opcode => panic!("unknown opcode `{}`", opcode),
-            }
-        }
-    }
-}
-
 fn part1(input: Vec<i64>) -> i64 {
-    Computer::new(input).next(1).unwrap()
+    Computer::new(input).next(Some(1)).unwrap()
 }
 
 fn part2(input: Vec<i64>) -> i64 {
-    Computer::new(input).next(2).unwrap()
+    Computer::new(input).next(Some(2)).unwrap()
 }
 
 fn main() {
@@ -132,7 +29,7 @@ fn example1() {
     let mut computer = Computer::new(input.clone());
     let mut result = Vec::new();
     for _ in 0..input.len() {
-        result.push(computer.next(1).unwrap());
+        result.push(computer.next(None).unwrap());
     }
     assert_eq!(input, result);
 }
@@ -140,13 +37,13 @@ fn example1() {
 #[test]
 fn example2() {
     let input = parse_program("1102,34915192,34915192,7,4,7,99,0");
-    assert_eq!(Computer::new(input).next(1).unwrap(), 1219070632396864);
+    assert_eq!(Computer::new(input).next(None).unwrap(), 1219070632396864);
 }
 
 #[test]
 fn example3() {
     let input = parse_program("104,1125899906842624,99");
-    assert_eq!(Computer::new(input).next(1).unwrap(), 1125899906842624)
+    assert_eq!(Computer::new(input).next(None).unwrap(), 1125899906842624)
 }
 
 #[test]
