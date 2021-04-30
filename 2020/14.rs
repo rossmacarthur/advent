086c70f7764cca554981493a0a74d2a3c82174be
Ross MacArthur <ross@macarthur.io>
2021-04-30 19:05:26 +0200

    Add main function and tests for 2020 Day 14

--- 2020/14.rs
+++ 2020/14.rs
@@ -2,20 +2,10 @@ use std::collections::HashMap;
 
 use itertools::Itertools;
 
-const INPUT: &str = include_str!("input/14.txt");
-
-const DEFAULT_MASK: &str = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-
-#[derive(Debug)]
-pub enum Instruction<'a> {
-    Mask(&'a str),
-    Assign(u64, u64),
-}
-
 use Instruction::*;
 
-pub fn default_input() -> Vec<Instruction<'static>> {
-    INPUT
+fn parse_input<'a>(input: &'a str) -> Vec<Instruction<'a>> {
+    input
         .lines()
         .map(|line| match line.split(" = ").next_tuple().unwrap() {
             ("mask", mask) => Mask(mask),
@@ -32,25 +22,20 @@ pub fn default_input() -> Vec<Instruction<'static>> {
         .collect()
 }
 
-fn from_bin(s: &str) -> u64 {
-    u64::from_str_radix(s, 2).unwrap()
+fn default_input() -> Vec<Instruction<'static>> {
+    parse_input(include_str!("input/14.txt"))
 }
 
-pub fn part1(instrs: &[Instruction]) -> u64 {
-    let mut memory = HashMap::new();
-    let mut mask = DEFAULT_MASK;
-    for instr in instrs {
-        match instr {
-            Mask(m) => mask = m,
-            Assign(address, value) => {
-                let or = from_bin(&mask.replace('X', "0"));
-                let and = from_bin(&mask.replace('X', "1"));
-                let value = value | or & and;
-                memory.insert(*address, value);
-            }
-        }
-    }
-    memory.values().sum()
+const DEFAULT_MASK: &str = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+#[derive(Debug)]
+enum Instruction<'a> {
+    Mask(&'a str),
+    Assign(u64, u64),
+}
+
+fn from_bin(s: &str) -> u64 {
+    u64::from_str_radix(s, 2).unwrap()
 }
 
 fn combinations(address: u64, mask: &str, i: usize) -> Vec<u64> {
@@ -69,7 +54,24 @@ fn combinations(address: u64, mask: &str, i: usize) -> Vec<u64> {
         .collect()
 }
 
-pub fn part2(instrs: &[Instruction]) -> u64 {
+fn part1(instrs: &[Instruction]) -> u64 {
+    let mut memory = HashMap::new();
+    let mut mask = DEFAULT_MASK;
+    for instr in instrs {
+        match instr {
+            Mask(m) => mask = m,
+            Assign(address, value) => {
+                let or = from_bin(&mask.replace('X', "0"));
+                let and = from_bin(&mask.replace('X', "1"));
+                let value = (value | or) & and;
+                memory.insert(*address, value);
+            }
+        }
+    }
+    memory.values().sum()
+}
+
+fn part2(instrs: &[Instruction]) -> u64 {
     let mut memory = HashMap::new();
     let mut mask = DEFAULT_MASK;
     for instr in instrs {
@@ -83,4 +85,41 @@ pub fn part2(instrs: &[Instruction]) -> u64 {
         }
     }
     memory.values().sum()
+}
+
+fn main() {
+    let input = default_input();
+    let mut run = advent::start();
+    run.part(|| part1(&input));
+    run.part(|| part2(&input));
+    run.finish();
+}
+
+#[test]
+fn example1() {
+    let input = parse_input(
+        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0",
+    );
+    assert_eq!(part1(&input), 165);
+}
+
+#[test]
+fn example2() {
+    let input = parse_input(
+        "mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1",
+    );
+    assert_eq!(part2(&input), 208);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(&input), 11884151942312);
+    assert_eq!(part2(&input), 2625449018811);
 }
