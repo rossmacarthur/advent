76d4b52acf806342f48a28078447d61a5fe1c36d
Ross MacArthur <ross@macarthur.io>
2021-04-30 15:39:19 +0200

    Add main function and tests for 2020 Day 8

--- 2020/08.rs
+++ 2020/08.rs
@@ -1,9 +1,28 @@
 use itertools::Itertools;
 
-const INPUT: &str = include_str!("input/08.txt");
+fn parse_input(s: &str) -> Vec<(Operation, i64)> {
+    s.lines()
+        .map(|line| {
+            let (op, arg) = line.split_whitespace().next_tuple().unwrap();
+            (
+                match op {
+                    "nop" => Self::Nop,
+                    "acc" => Self::Acc,
+                    "jmp" => Self::Jmp,
+                    op => panic!("unknown operation `{}`", op),
+                },
+                arg.parse().unwrap(),
+            )
+        })
+        .collect()
+}
+
+fn default_input() -> Vec<(Operation, i64)> {
+    parse_input(include_str!("input/08.txt"))
+}
 
 #[derive(Debug, Clone, Copy)]
-pub enum Operation {
+enum Operation {
     Nop,
     Acc,
     Jmp,
@@ -15,25 +34,7 @@ struct ProgramResult {
     acc: i64,
 }
 
-pub fn default_input() -> Vec<(Operation, i64)> {
-    INPUT
-        .lines()
-        .map(|line| {
-            let (op, arg) = line.split_whitespace().next_tuple().unwrap();
-            (
-                match op {
-                    "nop" => Operation::Nop,
-                    "acc" => Operation::Acc,
-                    "jmp" => Operation::Jmp,
-                    op => panic!("unknown operation `{}`", op),
-                },
-                arg.parse().unwrap(),
-            )
-        })
-        .collect()
-}
-
-fn exec_boot_code(program: &[(Operation, i64)]) -> ProgramResult {
+fn exec(program: &[(Operation, i64)]) -> ProgramResult {
     let mut instrs = Vec::new();
     let mut acc = 0;
     let mut ptr = 0;
@@ -60,12 +61,12 @@ fn exec_boot_code(program: &[(Operation, i64)]) -> ProgramResult {
     }
 }
 
-pub fn part1(program: &[(Operation, i64)]) -> i64 {
-    exec_boot_code(program).acc
+fn part1(program: &[(Operation, i64)]) -> i64 {
+    exec(program).acc
 }
 
-pub fn part2(program: &[(Operation, i64)]) -> i64 {
-    let result = exec_boot_code(program);
+fn part2(program: &[(Operation, i64)]) -> i64 {
+    let result = exec(program);
     for ptr in result.instrs {
         let new_op = match program[ptr].0 {
             Operation::Nop => Operation::Jmp,
@@ -74,10 +75,42 @@ pub fn part2(program: &[(Operation, i64)]) -> i64 {
         };
         let mut new_program = program.to_vec();
         new_program[ptr].0 = new_op;
-        let result = exec_boot_code(&new_program);
+        let result = exec(&new_program);
         if result.is_finite {
             return result.acc;
         }
     }
     panic!("failed to fix infinite loop")
+}
+
+fn main() {
+    let input = default_input();
+    let mut run = advent::start();
+    run.part(|| part1(&input));
+    run.part(|| part2(&input));
+    run.finish();
+}
+
+#[test]
+fn example() {
+    let input = parse_input(
+        r#"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6"#,
+    );
+    assert_eq!(part1(&input), 5);
+    assert_eq!(part2(&input), 8);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(&input), 1584);
+    assert_eq!(part2(&input), 920);
 }
