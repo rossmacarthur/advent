35df3ab3c4debe2a76ce1ecdea43a4198a93b5d0
Ross MacArthur <ross@macarthur.io>
2021-12-02 10:02:11 +0200

    Add 2021 Day 2 solution

---
+++ 2021/02.rs
@@ -0,0 +1,78 @@
+fn parse_input(input: &str) -> Vec<(Command, i64)> {
+    input
+        .lines()
+        .map(|line| {
+            let (cmd, value) = line.split_once(char::is_whitespace).unwrap();
+            (
+                match cmd {
+                    "forward" => Command::Forward,
+                    "down" => Command::Down,
+                    "up" => Command::Up,
+                    d => panic!("unknown command `{}`", d),
+                },
+                value.parse().unwrap(),
+            )
+        })
+        .collect()
+}
+
+fn default_input() -> Vec<(Command, i64)> {
+    parse_input(include_str!("input/02.txt"))
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Command {
+    Forward,
+    Down,
+    Up,
+}
+
+fn part1(input: &[(Command, i64)]) -> i64 {
+    let (x, y) = input.iter().fold((0, 0), |(x, y), (cmd, v)| match cmd {
+        Command::Forward => (x + v, y),
+        Command::Down => (x, y + v),
+        Command::Up => (x, y - v),
+    });
+    x * y
+}
+
+fn part2(input: &[(Command, i64)]) -> i64 {
+    let (x, y, _) = input
+        .iter()
+        .fold((0, 0, 0), |(x, y, a), &(cmd, v)| match cmd {
+            Command::Forward => (x + v, y + a * v, a),
+            Command::Down => (x, y, a + v),
+            Command::Up => (x, y, a - v),
+        });
+    x * y
+}
+
+fn main() {
+    let input = default_input();
+    let mut run = advent::start();
+    run.part(|| part1(&input));
+    run.part(|| part2(&input));
+    run.finish();
+}
+
+#[test]
+fn example() {
+    let input = parse_input(
+        "\
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2",
+    );
+    assert_eq!(part1(&input), 150);
+    assert_eq!(part2(&input), 900);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(&input), 1654760);
+    assert_eq!(part2(&input), 1956047400);
+}
