6342a9a3f1a3004c7842d12e4ea7d89bd0db43e6
Ross MacArthur <ross@macarthur.io>
2021-12-03 09:50:44 +0200

    Add 2021 Day 3 solution

---
+++ 2021/03.rs
@@ -0,0 +1,90 @@
+fn parse_input(input: &str) -> Vec<i64> {
+    input
+        .split_whitespace()
+        .map(|s| i64::from_str_radix(s, 2))
+        .map(Result::unwrap)
+        .collect()
+}
+
+fn default_input() -> Vec<i64> {
+    parse_input(include_str!("input/03.txt"))
+}
+
+fn partition(values: &[i64], bit: i64) -> (Vec<i64>, Vec<i64>) {
+    values.iter().partition(|&v| (v & 1 << bit) > 0)
+}
+
+fn criteria<F>(values: &[i64], bits: i64, cmp: F) -> i64
+where
+    F: Fn(usize, usize) -> bool,
+{
+    let mut values = values.to_vec();
+    for bit in (0..bits).rev() {
+        let (ones, zeros) = partition(&values, bit);
+        if cmp(ones.len(), zeros.len()) {
+            values = ones;
+        } else {
+            values = zeros;
+        }
+        if let [value] = *values {
+            return value;
+        }
+    }
+    unreachable!()
+}
+
+fn part1(values: &[i64], bits: i64) -> i64 {
+    let mut gamma = 0;
+    let mut epsilon = 0;
+    for bit in 0..bits {
+        let (ones, zeros) = partition(values, bit);
+        if ones.len() > zeros.len() {
+            epsilon |= 1 << bit;
+        } else {
+            gamma |= 1 << bit;
+        }
+    }
+    gamma * epsilon
+}
+
+fn part2(values: &[i64], bits: i64) -> i64 {
+    let o2 = criteria(values, bits, |ones, zeros| ones >= zeros);
+    let co2 = criteria(values, bits, |ones, zeros| ones < zeros);
+    o2 * co2
+}
+
+fn main() {
+    let input = default_input();
+    let mut run = advent::start();
+    run.part(|| part1(&input, 12));
+    run.part(|| part2(&input, 12));
+    run.finish();
+}
+
+#[test]
+fn example() {
+    let input = parse_input(
+        "\
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010",
+    );
+    assert_eq!(part1(&input, 5), 198);
+    assert_eq!(part2(&input, 5), 230);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(&input, 12), 3958484);
+    assert_eq!(part2(&input, 12), 1613181);
+}
