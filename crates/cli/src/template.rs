5b21aabd611553d3a01aa68b17ec3d92c7ec48ed
Ross MacArthur <ross@macarthur.io>
2021-12-11 12:16:41 +0200

    Update template

--- crates/cli/src/template.rs
+++ crates/cli/src/template.rs
@@ -6,25 +6,24 @@ fn default_input() -> Vec<i64> {
     parse_input(include_str!("input/{day}.txt"))
 }
 
-fn part1(input: &[i64]) -> i64 {
+fn part1(input: Vec<i64>) -> i64 {
     todo!()
 }
 
-fn part2(input: &[i64]) -> i64 {
+fn part2(input: Vec<i64>) -> i64 {
     todo!()
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 1);
-    assert_eq!(part2(&input), 2);
+    assert_eq!(part1(input.clone()), 1);
+    assert_eq!(part2(input), 2);
 }
