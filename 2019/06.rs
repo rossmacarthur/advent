cac2aa067249668e7097e3b14c2ac31fb7deeee6
Ross MacArthur <ross@macarthur.io>
2021-12-28 20:19:29 +0200

    Use prelude in 2019 Day 6 solution

--- 2019/06.rs
+++ 2019/06.rs
@@ -1,7 +1,4 @@
-use std::collections::HashMap;
-use std::iter;
-
-use itertools::Itertools;
+use advent::prelude::*;
 
 type Orbits<'a> = HashMap<&'a str, &'a str>;
 
@@ -21,42 +18,41 @@ fn path<'a>(orbits: &'a Orbits<'_>, to: &'a str) -> Vec<&'a str> {
     iter::successors(Some(to), move |to| orbits.get(*to).copied()).collect()
 }
 
-fn part1(orbits: &Orbits<'_>) -> usize {
-    orbits.keys().map(|to| path(orbits, to).len() - 1).sum()
+fn part1(orbits: Orbits<'_>) -> usize {
+    orbits.keys().map(|to| path(&orbits, to).len() - 1).sum()
 }
 
-fn part2(orbits: &Orbits<'_>) -> usize {
-    let you = path(orbits, "YOU");
-    let san = path(orbits, "SAN");
+fn part2(orbits: Orbits<'_>) -> usize {
+    let you = path(&orbits, "YOU");
+    let san = path(&orbits, "SAN");
     you.iter().take_while(|x| !san.contains(x)).count()
         + san.iter().take_while(|x| !you.contains(x)).count()
         - 2
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example1() {
     let input = parse_input("COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L");
-    assert_eq!(part1(&input), 42);
+    assert_eq!(part1(input), 42);
 }
 
 #[test]
 fn example2() {
     let input =
         parse_input("COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN");
-    assert_eq!(part2(&input), 4);
+    assert_eq!(part2(input), 4);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 106065);
-    assert_eq!(part2(&input), 253);
+    assert_eq!(part1(input.clone()), 106065);
+    assert_eq!(part2(input), 253);
 }
