44fc89f7cabee6bd1c65b306c9eb28c1b361bb3c
Ross MacArthur <ross@macarthur.io>
2022-01-05 17:58:51 +0200

    Fix bench in 2019 Day 5 solution

--- 2019/05.rs
+++ 2019/05.rs
@@ -102,10 +102,9 @@ fn part2(input: Vec<i64>) -> i64 {
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(input.clone()));
-    run.part(|| part2(input.clone()));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
