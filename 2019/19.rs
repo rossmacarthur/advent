bffb57e102496915b7a65bd1259dff439bf24bbe
Ross MacArthur <ross@macarthur.io>
2022-01-05 19:02:43 +0200

    Use prelude in 2019 Day 19 solution

--- 2019/19.rs
+++ 2019/19.rs
@@ -1,32 +1,32 @@
 mod intcode;
 
-use itertools::iproduct;
-
+use advent::prelude::*;
 use intcode::{parse_program, Computer};
 
 fn default_input() -> Vec<i64> {
     parse_program(include_str!("input/19.txt"))
 }
 
-fn is_affected(input: &[i64], x: i64, y: i64) -> bool {
-    let mut computer = Computer::new(input.to_vec());
+fn is_affected(input: Vec<i64>, x: i64, y: i64) -> bool {
+    let mut computer = Computer::new(input);
     computer.input(x);
     computer.input(y);
     computer.next().unwrap() == 1
 }
 
-fn part1(input: &[i64]) -> usize {
-    iproduct!(0..50, 0..50)
-        .filter(|(x, y)| is_affected(input, *x, *y))
+fn part1(input: Vec<i64>) -> usize {
+    (0..50)
+        .cartesian_product(0..50)
+        .filter(|(x, y)| is_affected(input.clone(), *x, *y))
         .count()
 }
 
-fn part2(input: &[i64]) -> i64 {
+fn part2(input: Vec<i64>) -> i64 {
     const D: i64 = 99;
     let mut x = 0;
     let mut y = D;
     loop {
-        while !is_affected(input, x, y) {
+        while !is_affected(input.clone(), x, y) {
             x += 1;
         }
         // Check the other 3 corners are within the beam:
@@ -39,7 +39,7 @@ fn part2(input: &[i64]) -> i64 {
         if [(x, y - D), (x + D, y - D), (x + D, y)]
             .iter()
             .copied()
-            .all(|(x, y)| is_affected(input, x, y))
+            .all(|(x, y)| is_affected(input.clone(), x, y))
         {
             break x * 10_000 + (y - D);
         }
@@ -48,16 +48,15 @@ fn part2(input: &[i64]) -> i64 {
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 192);
-    assert_eq!(part2(&input), 8381082);
+    assert_eq!(part1(input.clone()), 192);
+    assert_eq!(part2(input), 8381082);
 }
