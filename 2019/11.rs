29443643602b062f63172cc80f2e5ed9ec50dad8
Ross MacArthur <ross@macarthur.io>
2022-01-10 15:22:51 +0200

    Use prelude in 2019 Day 11 solution

--- 2019/11.rs
+++ 2019/11.rs
@@ -1,12 +1,7 @@
 mod intcode;
 
-use std::collections::BTreeMap;
-
-use vectrix::{vector, Vector2, VectorExt};
-
+use advent::prelude::*;
 use intcode::{parse_program, Computer};
-
-type Vector = Vector2<i64>;
 
 fn default_input() -> Vec<i64> {
     parse_program(include_str!("input/11.txt"))
@@ -24,83 +19,84 @@ enum Turn {
     Right,
 }
 
-use Color::*;
-use Turn::*;
-
 impl Computer {
     fn next_color_and_turn(&mut self, color: Color) -> Option<(Color, Turn)> {
         self.input(color as i64);
         let color = self.next().map(|v| match v {
-            0 => Black,
-            1 => White,
+            0 => Color::Black,
+            1 => Color::White,
             c => panic!("invalid color `{}`", c),
         })?;
         let turn = self.next().map(|v| match v {
-            0 => Left,
-            1 => Right,
+            0 => Turn::Left,
+            1 => Turn::Right,
             t => panic!("invalid turn `{}`", t),
         })?;
         Some((color, turn))
     }
 }
 
-fn rotate(vector: Vector, turn: Turn) -> Vector {
-    match turn {
-        Left => vector.rotated(90),
-        Right => vector.rotated(-90),
-    }
-}
-
-fn paint(program: Vec<i64>, color: Color) -> BTreeMap<Vector, Color> {
+fn paint(program: Vec<i64>, color: Color) -> BTreeMap<Vector2, Color> {
     let mut computer = Computer::new(program);
     let mut map = BTreeMap::new();
-    let mut position = Vector::zero();
-    let mut direction = vector![0, 1];
-    while let Some((color, turn)) =
-        computer.next_color_and_turn(*map.get(&position).unwrap_or(&color))
-    {
-        map.insert(position, color);
-        direction = rotate(direction, turn);
-        position += direction;
+    let mut p = Vector2::zero();
+    let mut d = vector![0, 1];
+    while let Some((color, turn)) = computer.next_color_and_turn(*map.get(&p).unwrap_or(&color)) {
+        map.insert(p, color);
+        d = match turn {
+            Turn::Left => vector![-d.y, d.x],
+            Turn::Right => vector![d.y, -d.x],
+        };
+        p += d;
     }
     map
 }
 
 fn part1(program: Vec<i64>) -> usize {
-    paint(program, Black).len()
+    paint(program, Color::Black).len()
 }
 
 fn part2(program: Vec<i64>) -> String {
-    let map = paint(program, White);
+    let map = paint(program, Color::White);
 
     let min_x = map.keys().map(|v| v.x).min().unwrap();
     let max_x = map.keys().map(|v| v.x).max().unwrap();
     let min_y = map.keys().map(|v| v.y).min().unwrap();
     let max_y = map.keys().map(|v| v.y).max().unwrap();
 
-    let mut result = String::from('\n');
+    let mut result = String::new();
     for y in (min_y..=max_y).rev() {
         for x in min_x..=max_x {
-            result.push_str(match map.get(&vector![x, y]) {
-                Some(Black) | None => "  ",
-                Some(White) => "██",
-            })
+            result += match map.get(&vector![x, y]) {
+                Some(Color::Black) | None => "░░",
+                Some(Color::White) => "██",
+            };
         }
-        result.push('\n');
+        result += "\n"
     }
     result
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(input.clone()));
-    run.part(|| part2(input.clone()));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(input), 1930);
+    assert_eq!(part1(input.clone()), 1930);
+    assert_eq!(
+        part2(input),
+        "\
+░░██████░░░░████████░░██░░░░██░░██░░░░██░░████████░░░░████░░░░████████░░██░░░░██░░░░░░
+░░██░░░░██░░██░░░░░░░░██░░██░░░░██░░░░██░░██░░░░░░░░██░░░░██░░░░░░░░██░░██░░░░██░░░░░░
+░░██░░░░██░░██████░░░░████░░░░░░████████░░██████░░░░██░░░░░░░░░░░░██░░░░██░░░░██░░░░░░
+░░██████░░░░██░░░░░░░░██░░██░░░░██░░░░██░░██░░░░░░░░██░░░░░░░░░░██░░░░░░██░░░░██░░░░░░
+░░██░░░░░░░░██░░░░░░░░██░░██░░░░██░░░░██░░██░░░░░░░░██░░░░██░░██░░░░░░░░██░░░░██░░░░░░
+░░██░░░░░░░░██░░░░░░░░██░░░░██░░██░░░░██░░████████░░░░████░░░░████████░░░░████░░░░░░░░
+"
+    )
 }
