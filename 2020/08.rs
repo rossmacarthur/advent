6c76a81b3289f9253a07633fdd08b2aa0bac2d76
Ross MacArthur <ross@macarthur.io>
2022-01-11 17:36:17 +0200

    Use prelude in 2020 Day 8 solution

--- 2020/08.rs
+++ 2020/08.rs
@@ -1,100 +1,98 @@
-use itertools::Itertools;
+use advent::prelude::*;
 
-fn parse_input(s: &str) -> Vec<(Operation, i64)> {
+fn parse_input(s: &str) -> Vec<(Op, i64)> {
     s.lines()
         .map(|line| {
-            let (op, arg) = line.split_whitespace().next_tuple().unwrap();
-            (
-                match op {
-                    "nop" => Operation::Nop,
-                    "acc" => Operation::Acc,
-                    "jmp" => Operation::Jmp,
-                    op => panic!("unknown operation `{}`", op),
-                },
-                arg.parse().unwrap(),
-            )
+            let [op, arg] = line.split_whitespace().next_array().unwrap();
+            let op = match op {
+                "nop" => Op::Nop,
+                "acc" => Op::Acc,
+                "jmp" => Op::Jmp,
+                op => panic!("unknown operation `{}`", op),
+            };
+            (op, arg.parse().unwrap())
         })
         .collect()
 }
 
-fn default_input() -> Vec<(Operation, i64)> {
+fn default_input() -> Vec<(Op, i64)> {
     parse_input(include_str!("input/08.txt"))
 }
 
 #[derive(Debug, Clone, Copy)]
-enum Operation {
+enum Op {
     Nop,
     Acc,
     Jmp,
 }
 
-struct ProgramResult {
+#[derive(Debug, Clone)]
+struct Result {
     is_finite: bool,
     instrs: Vec<usize>,
     acc: i64,
 }
 
-fn exec(program: &[(Operation, i64)]) -> ProgramResult {
+fn exec(prog: &[(Op, i64)]) -> Result {
     let mut instrs = Vec::new();
-    let mut acc = 0;
     let mut ptr = 0;
-    while !instrs.contains(&ptr) && ptr < program.len() {
+    let mut acc = 0;
+    while !instrs.contains(&ptr) && ptr < prog.len() {
         instrs.push(ptr);
-        let (op, arg) = program[ptr];
-        match op {
-            Operation::Nop => {
+        match prog[ptr] {
+            (Op::Nop, _) => {
                 ptr += 1;
             }
-            Operation::Acc => {
-                acc += arg;
+            (Op::Acc, inc) => {
                 ptr += 1;
+                acc += inc;
             }
-            Operation::Jmp => {
-                ptr = (ptr as i64 + arg) as usize;
+            (Op::Jmp, offset) => {
+                ptr = (ptr as i64 + offset) as usize;
             }
         }
     }
-    ProgramResult {
-        is_finite: ptr == program.len(),
+    Result {
+        is_finite: ptr == prog.len(),
         instrs,
         acc,
     }
 }
 
-fn part1(program: &[(Operation, i64)]) -> i64 {
-    exec(program).acc
+fn part1(prog: Vec<(Op, i64)>) -> i64 {
+    exec(&prog).acc
 }
 
-fn part2(program: &[(Operation, i64)]) -> i64 {
-    let result = exec(program);
-    for ptr in result.instrs {
-        let new_op = match program[ptr].0 {
-            Operation::Nop => Operation::Jmp,
-            Operation::Jmp => Operation::Nop,
+fn part2(mut prog: Vec<(Op, i64)>) -> i64 {
+    for ptr in exec(&prog).instrs {
+        let old = prog[ptr].0;
+        let new = match old {
+            Op::Nop => Op::Jmp,
+            Op::Jmp => Op::Nop,
             _ => continue,
         };
-        let mut new_program = program.to_vec();
-        new_program[ptr].0 = new_op;
-        let result = exec(&new_program);
-        if result.is_finite {
-            return result.acc;
+        prog[ptr].0 = new;
+        let Result { is_finite, acc, .. } = exec(&prog);
+        if is_finite {
+            return acc;
         }
+        prog[ptr].0 = old;
     }
     panic!("failed to fix infinite loop")
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example() {
     let input = parse_input(
-        r#"nop +0
+        "\
+nop +0
 acc +1
 jmp +4
 acc +3
@@ -102,15 +100,15 @@ jmp -3
 acc -99
 acc +1
 jmp -4
-acc +6"#,
+acc +6",
     );
-    assert_eq!(part1(&input), 5);
-    assert_eq!(part2(&input), 8);
+    assert_eq!(part1(input.clone()), 5);
+    assert_eq!(part2(input), 8);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 1584);
-    assert_eq!(part2(&input), 920);
+    assert_eq!(part1(input.clone()), 1584);
+    assert_eq!(part2(input), 920);
 }
