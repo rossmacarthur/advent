d432ffd02c928c8129f7ce0f900651cfc64ee662
Ross MacArthur <ross@macarthur.io>
2022-01-11 14:04:58 +0200

    Use prelude in 2020 Day 3 solution

--- 2020/03.rs
+++ 2020/03.rs
@@ -1,53 +1,48 @@
-use std::collections::HashSet;
+use advent::prelude::*;
 
-use vectrix::{parse_map_set, vector, Vector2};
-
-type Vector = Vector2<i64>;
-
-fn default_input() -> HashSet<Vector> {
+fn default_input() -> HashSet<Vector2> {
     parse_map_set(include_str!("input/03.txt"))
 }
 
-fn count_trees_for_slope(map: &HashSet<Vector>, slope: Vector) -> usize {
+fn count_trees(map: &HashSet<Vector2>, slope: Vector2) -> usize {
     let len_x = map.iter().map(|v| v.x).max().unwrap() + 1;
     let len_y = map.iter().map(|v| v.y).max().unwrap() + 1;
     let mut trees = 0;
-    let mut location = Vector::zero();
-    while location.y < len_y {
-        if map.contains(&location) {
+    let mut p = Vector2::zero();
+    while p.y < len_y {
+        if map.contains(&p) {
             trees += 1;
         }
-        location += slope;
-        location.x %= len_x;
+        p += slope;
+        p.x %= len_x;
     }
     trees
 }
 
-fn part1(map: &HashSet<Vector>) -> usize {
-    count_trees_for_slope(map, vector![3, 1])
+fn part1(map: HashSet<Vector2>) -> usize {
+    count_trees(&map, vector![3, 1])
 }
 
-fn part2(map: &HashSet<Vector>) -> usize {
+fn part2(map: HashSet<Vector2>) -> usize {
     [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-        .iter()
-        .copied()
-        .map(Vector::from)
-        .map(|slope| count_trees_for_slope(map, slope))
+        .into_iter()
+        .map(Vector2::from)
+        .map(|slope| count_trees(&map, slope))
         .product()
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example() {
     let input = parse_map_set(
-        r#"..##.......
+        "\
+..##.......
 #...#...#..
 .#....#..#.
 ..#.#...#.#
@@ -57,15 +52,15 @@ fn example() {
 .#........#
 #.##...#...
 #...##....#
-.#..#...#.#"#,
+.#..#...#.#",
     );
-    assert_eq!(part1(&input), 7);
-    assert_eq!(part2(&input), 336);
+    assert_eq!(part1(input.clone()), 7);
+    assert_eq!(part2(input), 336);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 282);
-    assert_eq!(part2(&input), 958815792);
+    assert_eq!(part1(input.clone()), 282);
+    assert_eq!(part2(input), 958815792);
 }
