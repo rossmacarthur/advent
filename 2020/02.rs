2131fb3da58724377732c9a96aa089b348204891
Ross MacArthur <ross@macarthur.io>
2022-01-11 14:04:09 +0200

    Use prelude in 2020 Day 2 solution

--- 2020/02.rs
+++ 2020/02.rs
@@ -1,37 +1,32 @@
-use regex_macro::regex;
+use advent::prelude::*;
 
-fn parse_input(s: &str) -> Vec<Element> {
+fn parse_input(s: &str) -> Vec<Element<'_>> {
     regex!(r"(?P<lower>\d+)-(?P<upper>\d+)\s(?P<letter>.):\s(?P<password>.*)")
         .captures_iter(s)
-        .map(|caps| {
-            let lower = caps["lower"].parse().unwrap();
-            let upper = caps["upper"].parse().unwrap();
-            let letter = caps["letter"].parse().unwrap();
-            let password = caps["password"].to_owned();
-            Element {
-                lower,
-                upper,
-                letter,
-                password,
-            }
+        .map(|caps| Element {
+            lower: caps["lower"].parse().unwrap(),
+            upper: caps["upper"].parse().unwrap(),
+            letter: caps["letter"].parse().unwrap(),
+            password: caps.name("password").unwrap().as_str(),
         })
         .collect()
 }
 
-fn default_input() -> Vec<Element> {
+fn default_input() -> Vec<Element<'static>> {
     parse_input(include_str!("input/02.txt"))
 }
 
-struct Element {
+#[derive(Debug, Clone)]
+struct Element<'a> {
     lower: usize,
     upper: usize,
     letter: char,
-    password: String,
+    password: &'a str,
 }
 
-fn part1(elements: &[Element]) -> usize {
+fn part1(elements: Vec<Element<'_>>) -> usize {
     elements
-        .iter()
+        .into_iter()
         .filter(|e| {
             let freq = e.password.chars().filter(|&c| c == e.letter).count();
             (e.lower..=e.upper).contains(&freq)
@@ -39,9 +34,9 @@ fn part1(elements: &[Element]) -> usize {
         .count()
 }
 
-fn part2(elements: &[Element]) -> usize {
+fn part2(elements: Vec<Element<'_>>) -> usize {
     elements
-        .iter()
+        .into_iter()
         .filter(|e| {
             (e.password.chars().nth(e.lower - 1).unwrap() == e.letter)
                 ^ (e.password.chars().nth(e.upper - 1).unwrap() == e.letter)
@@ -50,26 +45,26 @@ fn part2(elements: &[Element]) -> usize {
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example() {
     let input = parse_input(
-        "1-3 a: abcde\n\
-         1-3 b: cdefg\n\
-         2-9 c: ccccccccc",
+        "\
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc",
     );
-    assert_eq!(part1(&input), 2);
-    assert_eq!(part2(&input), 1);
+    assert_eq!(part1(input.clone()), 2);
+    assert_eq!(part2(input), 1);
 }
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 383);
-    assert_eq!(part2(&input), 272);
+    assert_eq!(part1(input.clone()), 383);
+    assert_eq!(part2(input), 272);
 }
