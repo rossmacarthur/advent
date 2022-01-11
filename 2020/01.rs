60f5aafe8e280425680ec3218bc376ce036086e5
Ross MacArthur <ross@macarthur.io>
2022-01-11 13:43:43 +0200

    Use prelude in 2020 Day 1 solution

--- 2020/01.rs
+++ 2020/01.rs
@@ -1,66 +1,62 @@
-use std::cmp::Ordering::*;
+use advent::prelude::*;
 
-const SUM: u64 = 2020;
+const SUM: i64 = 2020;
 
-fn default_input() -> Vec<u64> {
+fn default_input() -> Vec<i64> {
     include_str!("input/01.txt")
         .lines()
         .map(str::parse)
         .map(Result::unwrap)
+        .sorted_unstable()
         .collect()
 }
 
-fn solve(numbers: &[u64], mut left: usize, c: u64) -> Option<u64> {
-    let mut right = numbers.len() - 1;
-    while left < right {
-        let a = numbers[left];
-        let b = numbers[right];
+fn solve(numbers: &[i64], mut i: usize, c: i64) -> Option<i64> {
+    let mut j = numbers.len() - 1;
+    while i < j {
+        let a = numbers[i];
+        let b = numbers[j];
         let sum = a + b + c;
         match sum.cmp(&SUM) {
-            Less => left += 1,
-            Greater => right -= 1,
-            Equal => return Some(a * b),
+            Ordering::Less => i += 1,
+            Ordering::Greater => j -= 1,
+            Ordering::Equal => return Some(a * b),
         }
     }
     None
 }
 
-fn part1(input: &[u64]) -> u64 {
-    let mut numbers = input.to_vec();
-    numbers.sort_unstable();
+fn part1(numbers: Vec<i64>) -> i64 {
     solve(&numbers, 0, 0).unwrap()
 }
 
-fn part2(input: &[u64]) -> u64 {
-    let mut numbers = input.to_vec();
-    numbers.sort_unstable();
+fn part2(numbers: Vec<i64>) -> i64 {
     for i in 0..numbers.len() - 2 {
         let c = numbers[i];
-        if let Some(a_x_b) = solve(&numbers, i + 1, c) {
-            return a_x_b * c;
+        if let Some(ab) = solve(&numbers, i + 1, c) {
+            return ab * c;
         }
     }
     unreachable!()
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example() {
     let input = vec![1721, 979, 366, 299, 675, 1456];
-    assert_eq!(part1(&input), 514579);
-    assert_eq!(part2(&input), 241861950);
+    assert_eq!(part1(input.clone()), 514579);
+    assert_eq!(part2(input), 241861950);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 567171);
-    assert_eq!(part2(&input), 212428694);
+    assert_eq!(part1(input.clone()), 567171);
+    assert_eq!(part2(input), 212428694);
 }
