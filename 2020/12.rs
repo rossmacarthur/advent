debe0fa0037f7f9ed676d0a07608093fdb5fd54a
Ross MacArthur <ross@macarthur.io>
2022-01-12 10:41:16 +0200

    Use prelude in 2020 Day 12 solution

--- 2020/12.rs
+++ 2020/12.rs
@@ -1,95 +1,89 @@
-use vectrix::{vector, Vector2, VectorExt};
+use advent::prelude::*;
 
-use Instruction::*;
-
-type Vector = Vector2<i64>;
-
-fn parse_input(input: &str) -> Vec<Instruction> {
+fn parse_input(input: &str) -> Vec<Instr> {
     input
         .split_whitespace()
         .map(|cmd| {
-            let (op, value) = cmd.split_at(1);
-            let value = value.parse().unwrap();
+            let (op, v) = cmd.split_at(1);
+            let v = v.parse().unwrap();
             match op {
-                "N" => Move(vector![0, value]),
-                "S" => Move(vector![0, -value]),
-                "E" => Move(vector![value, 0]),
-                "W" => Move(vector![-value, 0]),
-                "L" => Turn(value),
-                "R" => Turn(-value),
-                "F" => Forward(value),
+                "N" => Instr::Move(vector![0, v]),
+                "S" => Instr::Move(vector![0, -v]),
+                "E" => Instr::Move(vector![v, 0]),
+                "W" => Instr::Move(vector![-v, 0]),
+                "L" => Instr::Turn(v),
+                "R" => Instr::Turn(-v),
+                "F" => Instr::Forward(v),
                 op => panic!("unexpected operation `{}`", op),
             }
         })
         .collect()
 }
 
-fn default_input() -> Vec<Instruction> {
+fn default_input() -> Vec<Instr> {
     parse_input(include_str!("input/12.txt"))
 }
 
-enum Instruction {
-    Move(Vector),
+#[derive(Debug, Clone, Copy)]
+enum Instr {
+    Move(Vector2),
     Turn(i64),
     Forward(i64),
 }
 
-fn part1(instrs: &[Instruction]) -> i64 {
-    let mut ship = Vector::zero();
-    let mut direction = vector![1, 0];
+fn rotate(v: Vector2, a: i64) -> Vector2 {
+    match a.rem_euclid(360) {
+        0 => vector![v.x, v.y],
+        90 => vector![-v.y, v.x],
+        180 => vector![-v.x, -v.y],
+        270 => vector![v.y, -v.x],
+        a => panic!("unsupported oblique angle `{}`", a),
+    }
+}
+
+fn part1(instrs: Vec<Instr>) -> i64 {
+    let mut ship = Vector2::zero();
+    let mut d = vector![1, 0];
     for instr in instrs {
         match instr {
-            Move(vector) => {
-                ship += vector;
-            }
-            Turn(angle) => {
-                direction = direction.rotated(*angle);
-            }
-            Forward(distance) => {
-                ship += direction * distance;
-            }
+            Instr::Move(v) => ship += v,
+            Instr::Turn(a) => d = rotate(d, a),
+            Instr::Forward(dist) => ship += d * dist,
         }
     }
     ship.l1_norm()
 }
 
-fn part2(instrs: &[Instruction]) -> i64 {
-    let mut ship = Vector::zero();
-    let mut direction = vector![10, 1];
+fn part2(instrs: Vec<Instr>) -> i64 {
+    let mut ship = Vector2::zero();
+    let mut d = vector![10, 1];
     for instr in instrs {
         match instr {
-            Move(vector) => {
-                direction += vector;
-            }
-            Turn(angle) => {
-                direction = direction.rotated(*angle);
-            }
-            Forward(distance) => {
-                ship += direction * distance;
-            }
+            Instr::Move(v) => d += v,
+            Instr::Turn(a) => d = rotate(d, a),
+            Instr::Forward(dist) => ship += d * dist,
         }
     }
     ship.l1_norm()
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example() {
     let input = parse_input("F10 N3 F7 R90 F11");
-    assert_eq!(part1(&input), 25);
-    assert_eq!(part2(&input), 286);
+    assert_eq!(part1(input.clone()), 25);
+    assert_eq!(part2(input), 286);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 845);
-    assert_eq!(part2(&input), 27016);
+    assert_eq!(part1(input.clone()), 845);
+    assert_eq!(part2(input), 27016);
 }
