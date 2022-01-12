0b1dd85e655ba8708e3ec2b6a608a0cba8f43ef9
Ross MacArthur <ross@macarthur.io>
2022-01-12 22:12:33 +0200

    Use prelude in 2020 Day 16 solution

--- 2020/16.rs
+++ 2020/16.rs
@@ -1,15 +1,9 @@
-use std::collections::HashSet;
-use std::ops::RangeInclusive;
+use advent::prelude::*;
 
-use regex_macro::regex;
+type Ticket = Vec<i64>;
 
-use itertools::Itertools;
-
-type Ticket = Vec<u64>;
-type Input = (Vec<Rule>, Ticket, Vec<Ticket>);
-
-fn parse_tickets(lines: &str) -> impl Iterator<Item = Ticket> + '_ {
-    lines
+fn parse_tickets(input: &str) -> impl Iterator<Item = Ticket> + '_ {
+    input
         .lines()
         .skip(1) // header
         .map(|line| {
@@ -20,115 +14,106 @@ fn parse_tickets(lines: &str) -> impl Iterator<Item = Ticket> + '_ {
         })
 }
 
-fn parse_input(input: &str) -> Input {
-    let (rules, mine, nearby) = input.split("\n\n").next_tuple().unwrap();
-    let rules = rules
-        .lines()
-        .map(|line| {
-            let caps = regex!(r"(.+): ((\d+)\-(\d+)) or ((\d+)\-(\d+))")
-                .captures(line)
-                .unwrap();
-            let left_min = caps[3].parse().unwrap();
-            let left_max = caps[4].parse().unwrap();
-            let right_min = caps[6].parse().unwrap();
-            let right_max = caps[7].parse().unwrap();
-            Rule {
-                name: caps.get(1).unwrap().as_str().to_string(),
-                range: (left_min..=left_max, right_min..=right_max),
-            }
-        })
-        .collect();
-    let my_ticket = parse_tickets(mine).next().unwrap();
-    let nearby_tickets = parse_tickets(nearby).collect();
-    (rules, my_ticket, nearby_tickets)
+fn parse_input(input: &str) -> (Vec<Rule<'_>>, Ticket, Vec<Ticket>) {
+    let [rules, your, nearby] = input.split("\n\n").next_array().unwrap();
+    let rules =
+        regex!(r"(?P<name>.+): (?P<lmin>\d+)\-(?P<lmax>\d+) or (?P<rmin>\d+)\-(?P<rmax>\d+)")
+            .captures_iter(rules)
+            .map(|caps| Rule {
+                name: caps.name("name").unwrap().as_str(),
+                lmin: caps["lmin"].parse().unwrap(),
+                lmax: caps["lmax"].parse().unwrap(),
+                rmin: caps["rmin"].parse().unwrap(),
+                rmax: caps["rmax"].parse().unwrap(),
+            })
+            .collect();
+    let your = parse_tickets(your).next().unwrap();
+    let nearby = parse_tickets(nearby).collect();
+    (rules, your, nearby)
 }
 
-fn default_input() -> Input {
+fn default_input() -> (Vec<Rule<'static>>, Ticket, Vec<Ticket>) {
     parse_input(include_str!("input/16.txt"))
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Rule {
-    name: String,
-    range: (RangeInclusive<u64>, RangeInclusive<u64>),
+struct Rule<'a> {
+    name: &'a str,
+    lmin: i64,
+    lmax: i64,
+    rmin: i64,
+    rmax: i64,
 }
 
-impl Rule {
-    fn matches(&self, value: u64) -> bool {
-        self.range.0.contains(&value) || self.range.1.contains(&value)
-    }
-
-    fn matches_all(&self, values: &[u64]) -> bool {
-        values.iter().all(|value| self.matches(*value))
+impl Rule<'_> {
+    fn matches(&self, v: &i64) -> bool {
+        (self.lmin..=self.lmax).contains(v) || (self.rmin..=self.rmax).contains(v)
     }
 }
 
-fn part1(input: &Input) -> u64 {
-    let (rules, _, nearby_tickets) = input;
-    nearby_tickets
+fn part1((rules, _, nearby): (Vec<Rule<'_>>, Ticket, Vec<Ticket>)) -> i64 {
+    nearby
         .iter()
         .flat_map(|ticket| {
             ticket
                 .iter()
-                .filter(|&value| !rules.iter().any(|rule| rule.matches(*value)))
+                .filter(|v| !rules.iter().any(|rule| rule.matches(v)))
         })
         .sum()
 }
 
-fn part2(input: &Input) -> u64 {
-    let (rules, my_ticket, nearby_tickets) = input;
-
-    let valid_tickets: Vec<_> = nearby_tickets
+fn part2((rules, your, nearby): (Vec<Rule<'_>>, Ticket, Vec<Ticket>)) -> i64 {
+    // First we find all the tickets that match any rule correctly.
+    let valid: Vec<_> = nearby
         .iter()
         .filter(|ticket| {
             ticket
                 .iter()
-                .all(|value| rules.iter().any(|rule| rule.matches(*value)))
+                .all(|v| rules.iter().any(|rule| rule.matches(v)))
         })
         .collect();
 
-    let mut rule_sets: Vec<(_, HashSet<_>)> = (0..my_ticket.len())
+    // Now construct a map of column to set of rules that might apply.
+    let mut rule_sets: Vec<_> = (0..your.len())
         .map(|col| {
-            let set: Vec<_> = valid_tickets.iter().map(|ticket| ticket[col]).collect();
-            (
-                col,
-                rules
-                    .iter()
-                    .filter(|rule| rule.matches_all(&set))
-                    .cloned()
-                    .collect(),
-            )
+            // The column values for the valid tickets.
+            let set: Vec<_> = valid.iter().map(|ticket| ticket[col]).collect();
+            // The set of rules that match all the column values.
+            let rules: HashSet<_> = rules
+                .iter()
+                .filter(|rule| set.iter().all(|v| rule.matches(v)))
+                .collect();
+            (col, rules)
         })
+        .sorted_unstable_by_key(|(_, rules)| Reverse(rules.len()))
         .collect();
-    rule_sets.sort_by_key(|(_, rules)| rules.len());
 
     let mut result = 1;
-    for i in 0..rule_sets.len() {
-        let (col, rules) = rule_sets[i].clone();
+    while let Some((col, rules)) = rule_sets.pop() {
         assert_eq!(rules.len(), 1);
         let rule = rules.into_iter().next().unwrap();
         for (_, rule_set) in rule_sets.iter_mut() {
             rule_set.remove(&rule);
         }
         if rule.name.starts_with("departure") {
-            result *= my_ticket[col]
+            result *= your[col]
         }
     }
     result
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example() {
     let input = parse_input(
-        "class: 1-3 or 5-7
+        "\
+class: 1-3 or 5-7
 row: 6-11 or 33-44
 seat: 13-40 or 45-50
 
@@ -141,13 +126,13 @@ nearby tickets:
 55,2,20
 38,6,12",
     );
-    assert_eq!(part1(&input), 71);
-    assert_eq!(part2(&input), 1);
+    assert_eq!(part1(input.clone()), 71);
+    assert_eq!(part2(input), 1);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 29019);
-    assert_eq!(part2(&input), 517827547723);
+    assert_eq!(part1(input.clone()), 29019);
+    assert_eq!(part2(input), 517827547723);
 }
