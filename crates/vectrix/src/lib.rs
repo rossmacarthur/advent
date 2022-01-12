1fdfc0d614c1c13283cee4bbbf1e0a449ad09199
Ross MacArthur <ross@macarthur.io>
2022-01-12 10:41:16 +0200

    Use prelude in 2020 Day 12 solution

--- crates/vectrix/src/lib.rs
+++ crates/vectrix/src/lib.rs
@@ -19,49 +19,6 @@ pub const CARDINALS_8: &[Vector2<i64>] = &[
     NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST,
 ];
 
-pub trait VectorExt {
-    fn reduced(self) -> Self;
-    fn rotated(self, angle: i64) -> Self;
-    fn angle(&self) -> f64;
-}
-
-impl VectorExt for Vector2<i64> {
-    /// Returns the reduced row echelon form of the vector.
-    ///
-    /// This is the same as dividing each element by the greatest common
-    /// divisor of all the elements.
-    fn reduced(self) -> Self {
-        let x = self.x;
-        let y = self.y;
-        let div = gcd(x, y);
-        vector![x / div, y / div]
-    }
-
-    /// Returns the vector rotated around the origin.
-    ///
-    /// # Panics
-    ///
-    /// If the angle is not a multiple of 90 degrees.
-    fn rotated(self, angle: i64) -> Self {
-        let x = self.x;
-        let y = self.y;
-        match angle.rem_euclid(360) {
-            0 => vector![x, y],
-            90 => vector![-y, x],
-            180 => vector![-x, -y],
-            270 => vector![y, -x],
-            angle => panic!("called `Vector::rotate()` with oblique angle `{}`", angle),
-        }
-    }
-
-    /// Returns the angle in radians between the vector and the origin.
-    fn angle(&self) -> f64 {
-        (self.y as f64)
-            .atan2(self.x as f64)
-            .rem_euclid(std::f64::consts::TAU)
-    }
-}
-
 /// Returns the greatest common divisor of two numbers.
 pub fn gcd(mut x: i64, mut y: i64) -> i64 {
     while x != 0 {
