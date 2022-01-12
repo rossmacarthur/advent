faf7149060408f885b15e7f0f4c5cf1b794fe012
Ross MacArthur <ross@macarthur.io>
2022-01-12 09:58:17 +0200

    Use prelude in 2020 Day 10 solution

--- 2020/10.rs
+++ 2020/10.rs
@@ -1,6 +1,6 @@
-use itertools::Itertools;
+use advent::prelude::*;
 
-fn parse_input(input: &str) -> Vec<u64> {
+fn parse_input(input: &str) -> Vec<i64> {
     let mut j: Vec<_> = input
         .split_whitespace()
         .map(str::parse)
@@ -12,14 +12,14 @@ fn parse_input(input: &str) -> Vec<u64> {
     j
 }
 
-fn default_input() -> Vec<u64> {
+fn default_input() -> Vec<i64> {
     parse_input(include_str!("input/10.txt"))
 }
 
-fn part1(joltages: &[u64]) -> u64 {
+fn part1(joltages: Vec<i64>) -> i64 {
     let mut ones = 0;
     let mut threes = 0;
-    for (curr, next) in joltages.iter().tuple_windows() {
+    for [curr, next] in joltages.iter().array_windows() {
         match next - curr {
             1 => ones += 1,
             3 => threes += 1,
@@ -29,47 +29,45 @@ fn part1(joltages: &[u64]) -> u64 {
     ones * threes
 }
 
-fn part2(joltages: &[u64]) -> u64 {
+fn part2(joltages: Vec<i64>) -> i64 {
     let mut dp = vec![1];
     for (i, joltage) in joltages.iter().enumerate().skip(1) {
-        dp.push(
-            (i.saturating_sub(3)..i)
-                .filter(|j| joltage - joltages[*j] <= 3)
-                .map(|j| dp[j])
-                .sum(),
-        )
+        let sum = (i.saturating_sub(3)..i)
+            .filter(|j| joltage - joltages[*j] <= 3)
+            .map(|j| dp[j])
+            .sum();
+        dp.push(sum);
     }
     *dp.last().unwrap()
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example1() {
     let input = parse_input("16 10 15 5 1 11 7 19 6 12 4");
-    assert_eq!(part1(&input), 35);
-    assert_eq!(part2(&input), 8);
+    assert_eq!(part1(input.clone()), 35);
+    assert_eq!(part2(input), 8);
 }
 
 #[test]
 fn example2() {
     let input = parse_input(
-        "28 33 18 42 31 14 46 20 48 47 24 23 49 45
+        "28 33 18 42 31 14 46 20 48 47 24 23 49 45 \
          19 38 39 11 1 32 25 35 8 17 7 9 4 2 34 10 3",
     );
-    assert_eq!(part1(&input), 220);
-    assert_eq!(part2(&input), 19208);
+    assert_eq!(part1(input.clone()), 220);
+    assert_eq!(part2(input), 19208);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 1984);
-    assert_eq!(part2(&input), 3543369523456);
+    assert_eq!(part1(input.clone()), 1984);
+    assert_eq!(part2(input), 3543369523456);
 }
