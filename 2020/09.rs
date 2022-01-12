e1a18f2504e14beb7d2f12293ded510f3a0a5bbe
Ross MacArthur <ross@macarthur.io>
2022-01-12 08:58:14 +0200

    Use prelude in 2020 Day 9 solution

--- 2020/09.rs
+++ 2020/09.rs
@@ -1,6 +1,6 @@
-use std::cmp::Ordering::*;
+use advent::prelude::*;
 
-fn parse_input(input: &str) -> Vec<u64> {
+fn parse_input(input: &str) -> Vec<i64> {
     input
         .split_whitespace()
         .map(str::parse)
@@ -8,37 +8,39 @@ fn parse_input(input: &str) -> Vec<u64> {
         .collect()
 }
 
-fn default_input() -> Vec<u64> {
+fn default_input() -> Vec<i64> {
     parse_input(include_str!("input/09.txt"))
 }
 
-fn part1(input: &[u64], size: usize) -> u64 {
+fn part1(input: Vec<i64>, size: usize) -> i64 {
     input
         .windows(size + 1)
-        .find(|window| {
+        .find_map(|window| {
             for i in 0..(size - 1) {
                 for j in (i + 1)..size {
                     if window[i] + window[j] == window[size] {
-                        return false;
+                        return None;
                     }
                 }
             }
-            true
+            Some(window[size])
         })
-        .map(|window| window[size])
         .unwrap()
 }
 
-fn part2(input: &[u64], invalid: u64) -> u64 {
+fn part2(input: Vec<i64>, invalid: i64) -> i64 {
     let mut i = 0;
     let mut j = 1;
     while j < input.len() {
-        let sum: u64 = input[i..j].iter().sum();
+        let sum: i64 = input[i..j].iter().sum();
         match sum.cmp(&invalid) {
-            Less => j += 1,
-            Greater => i += 1,
-            Equal => {
-                return input[i..j].iter().max().unwrap() + input[i..j].iter().min().unwrap();
+            Ordering::Less => j += 1,
+            Ordering::Greater => i += 1,
+            Ordering::Equal => {
+                let [min, max] = input[i..j]
+                    .iter()
+                    .fold([i64::MAX, i64::MIN], |[a, z], &i| [min(a, i), max(z, i)]);
+                return min + max;
             }
         }
     }
@@ -46,10 +48,9 @@ fn part2(input: &[u64], invalid: u64) -> u64 {
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input, 25));
-    run.part(|| part2(&input, 70639851));
+    run.part(|| part1(default_input(), 25));
+    run.part(|| part2(default_input(), 70639851));
     run.finish();
 }
 
@@ -57,13 +58,13 @@ fn main() {
 fn example() {
     let input =
         parse_input("35 20 15 25 47 40 62 55 65 95 102 117 150 182 127 219 299 277 309 576");
-    assert_eq!(part1(&input, 5), 127);
-    assert_eq!(part2(&input, 127), 62);
+    assert_eq!(part1(input.clone(), 5), 127);
+    assert_eq!(part2(input, 127), 62);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input, 25), 70639851);
-    assert_eq!(part2(&input, 70639851), 8249240);
+    assert_eq!(part1(input.clone(), 25), 70639851);
+    assert_eq!(part2(input, 70639851), 8249240);
 }
