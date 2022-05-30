4d0285db82dd51894ce093594c7f878ba317c30c
Ross MacArthur <ross@macarthur.io>
2022-05-30 19:33:39 +0200

    Use prelude in 2020 Day 22 solution

--- 2020/22.rs
+++ 2020/22.rs
@@ -1,8 +1,4 @@
-use std::collections::{HashSet, VecDeque};
-
-use itertools::Itertools;
-
-type Input = (VecDeque<usize>, VecDeque<usize>);
+use advent::prelude::*;
 
 fn parse_player(s: &str) -> VecDeque<usize> {
     s.lines()
@@ -12,12 +8,12 @@ fn parse_player(s: &str) -> VecDeque<usize> {
         .collect()
 }
 
-fn default_input() -> Input {
-    let (one, two) = include_str!("input/22.txt")
+fn default_input() -> [VecDeque<usize>; 2] {
+    include_str!("input/22.txt")
         .split("\n\n")
-        .next_tuple()
-        .unwrap();
-    (parse_player(one), parse_player(two))
+        .map(parse_player)
+        .next_array()
+        .unwrap()
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -34,30 +30,54 @@ fn score(deck: VecDeque<usize>) -> usize {
         .sum()
 }
 
-fn combat(
-    mut deck1: VecDeque<usize>,
-    mut deck2: VecDeque<usize>,
-    recurse: bool,
-) -> (Player, VecDeque<usize>) {
-    let mut previous = HashSet::new();
+fn combat([mut deck1, mut deck2]: [VecDeque<usize>; 2]) -> VecDeque<usize> {
     loop {
-        if recurse {
-            if previous.contains(&(deck1.clone(), deck2.clone())) {
-                return (Player::One, deck1);
-            } else {
-                previous.insert((deck1.clone(), deck2.clone()));
-            }
-        }
-
         let card1 = deck1.pop_front().unwrap();
         let card2 = deck2.pop_front().unwrap();
 
-        let winner = if recurse && card1 <= deck1.len() && card2 <= deck2.len() {
-            let (winner, _) = combat(
-                deck1.iter().copied().take(card1).collect(),
-                deck2.iter().copied().take(card2).collect(),
-                recurse,
-            );
+        let winner = if card1 > card2 {
+            Player::One
+        } else if card1 < card2 {
+            Player::Two
+        } else {
+            unreachable!()
+        };
+
+        match winner {
+            Player::One => {
+                deck1.push_back(card1);
+                deck1.push_back(card2);
+                if deck2.is_empty() {
+                    return deck1;
+                }
+            }
+            Player::Two => {
+                deck2.push_back(card2);
+                deck2.push_back(card1);
+                if deck1.is_empty() {
+                    return deck2;
+                }
+            }
+        }
+    }
+}
+
+fn recursive_combat(mut decks: [VecDeque<usize>; 2]) -> (Player, VecDeque<usize>) {
+    let mut previous = HashSet::new();
+    loop {
+        if previous.contains(&decks) {
+            return (Player::One, decks[0].clone());
+        }
+        previous.insert(decks.clone());
+
+        let [deck1, deck2] = &mut decks;
+        let card1 = deck1.pop_front().unwrap();
+        let card2 = deck2.pop_front().unwrap();
+
+        let winner = if card1 <= deck1.len() && card2 <= deck2.len() {
+            let new1 = deck1.iter().copied().take(card1).collect();
+            let new2 = deck2.iter().copied().take(card2).collect();
+            let (winner, _) = recursive_combat([new1, new2]);
             winner
         } else if card1 > card2 {
             Player::One
@@ -72,58 +92,55 @@ fn combat(
                 deck1.push_back(card1);
                 deck1.push_back(card2);
                 if deck2.is_empty() {
-                    break (Player::One, deck1);
+                    return (Player::One, mem::take(deck1));
                 }
             }
             Player::Two => {
                 deck2.push_back(card2);
                 deck2.push_back(card1);
                 if deck1.is_empty() {
-                    break (Player::Two, deck2);
+                    return (Player::Two, mem::take(deck2));
                 }
             }
         }
     }
 }
 
-fn part1((one, two): &Input) -> usize {
-    let (_, deck) = combat(one.clone(), two.clone(), false);
+fn part1(decks: [VecDeque<usize>; 2]) -> usize {
+    let deck = combat(decks);
     score(deck)
 }
 
-fn part2((one, two): &Input) -> usize {
-    let (_, deck) = combat(one.clone(), two.clone(), true);
+fn part2(decks: [VecDeque<usize>; 2]) -> usize {
+    let (_, deck) = recursive_combat(decks);
     score(deck)
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
-}
-
-#[cfg(test)]
-macro_rules! deq {
-    ($($i:expr),*) => (VecDeque::from(vec![$($i),*]));
 }
 
 #[test]
 fn example1() {
-    let input = (deq![9, 2, 6, 3, 1], deq![5, 8, 4, 7, 10]);
-    assert_eq!(part1(&input), 306);
-    assert_eq!(part2(&input), 291);
+    let input = [
+        VecDeque::from([9, 2, 6, 3, 1]),
+        VecDeque::from([5, 8, 4, 7, 10]),
+    ];
+    assert_eq!(part1(input.clone()), 306);
+    assert_eq!(part2(input), 291);
 }
 
 #[test]
 fn example2() {
-    part2(&(deq![43, 19], deq![2, 29, 14]));
+    part2([VecDeque::from([43, 19]), VecDeque::from([2, 29, 14])]);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 34566);
-    assert_eq!(part2(&input), 31854);
+    assert_eq!(part1(input.clone()), 34566);
+    assert_eq!(part2(input), 31854);
 }
