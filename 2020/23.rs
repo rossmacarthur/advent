6d0ca03a3a41b9b3b6f878dd2c119571c1d144e0
Ross MacArthur <ross@macarthur.io>
2022-06-04 17:47:42 +0200

    Improve 2020 Day 23 solution
    Use a simple `Vec` for the cup circle instead of a `HashMap`, this
    improves the performance immensely.

--- 2020/23.rs
+++ 2020/23.rs
@@ -1,72 +1,94 @@
-use std::collections::HashMap;
-use std::iter;
+use advent::prelude::*;
 
-use itertools::Itertools;
-
-fn parse_input(s: &str) -> Vec<u64> {
-    s.chars().map(|c| c.to_digit(10).unwrap().into()).collect()
+fn parse_input(s: &str) -> Vec<usize> {
+    s.bytes().map(|b| usize::from(b - b'0')).collect()
 }
 
-fn default_input() -> Vec<u64> {
+fn default_input() -> Vec<usize> {
     parse_input("716892543")
 }
 
-fn play(cups: Vec<u64>, moves: usize, limit: Option<u64>) -> HashMap<u64, u64> {
-    let max = cups.iter().max().copied().unwrap();
-    let limit = limit.unwrap_or(max);
-    let mut curr = cups[0];
-    let mut circle: HashMap<_, _> = cups
-        .into_iter()
-        .chain(max + 1..=limit)
-        .chain(iter::once(curr))
-        .tuple_windows()
-        .collect();
-    for _ in 0..moves {
-        let cup_a = circle[&curr];
-        let cup_b = circle[&cup_a];
-        let cup_c = circle[&cup_b];
-        let mut dest = curr - 1;
-        while dest == 0 || [cup_a, cup_b, cup_c].contains(&dest) {
-            if dest == 0 {
-                dest = limit;
-            } else {
-                dest -= 1;
-            }
+fn play(cups: &[usize], moves: usize, n: Option<usize>) -> Vec<usize> {
+    let n = n.unwrap_or_else(|| cups.len());
+
+    let start = cups[0];
+
+    // Construct a list such that each value used as an index refers to the next
+    // value in the circle.
+    let mut circle = {
+        let mut circle = vec![0; n + 1];
+        let mut last = start;
+        for &c in &cups[1..] {
+            circle[last] = c;
+            last = c;
         }
-        circle.insert(curr, circle[&cup_c]);
-        circle.insert(cup_c, circle[&dest]);
-        circle.insert(dest, cup_a);
-        curr = circle[&curr];
+        for c in (cups.len() + 1)..n + 1 {
+            circle[last] = c;
+            last = c;
+        }
+        circle[last] = start;
+        circle
+    };
+
+    let mut curr = start;
+    for _ in 0..moves {
+        // Pick up the next three cups.
+        let a = circle[curr];
+        let b = circle[a];
+        let c = circle[b];
+
+        // Now find the destination cup.
+        let dest = {
+            let next = |d| if d > 1 { d - 1 } else { n };
+            let mut d = next(curr);
+            while [a, b, c].contains(&d) {
+                d = next(d);
+            }
+            d
+        };
+
+        // Place the three cups down after the destination cup.
+        {
+            let tmp = circle[curr];
+            circle[curr] = circle[c];
+            circle[c] = circle[dest];
+            circle[dest] = tmp;
+        }
+
+        // Finally select the next current cup.
+        curr = circle[curr];
     }
+
     circle
 }
 
-fn part1(cups: Vec<u64>) -> String {
-    let len = cups.len();
-    let circle = play(cups, 100, None);
-    iter::successors(Some(1), |cup| Some(circle[cup]))
-        .take(len)
-        .skip(1)
-        .map(|cup| cup.to_string())
-        .collect()
+fn iter(circle: &[usize]) -> impl Iterator<Item = usize> + '_ {
+    iter::successors(Some(1), |&c| Some(circle[c]))
 }
 
-fn part2(cups: Vec<u64>) -> u64 {
-    let circle = play(cups, 10_000_000, Some(1_000_000));
-    let cup = circle[&1];
-    cup * circle[&cup]
+fn part1(cups: Vec<usize>) -> String {
+    let circle = play(&cups, 100, None);
+    let result = iter(&circle)
+        .take(cups.len())
+        .skip(1)
+        .map(|c| b'0' + (c as u8))
+        .collect();
+    String::from_utf8(result).unwrap()
+}
+
+fn part2(cups: Vec<usize>) -> usize {
+    let circle = play(&cups, 10_000_000, Some(1_000_000));
+    iter(&circle).skip(1).take(2).product()
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(input.clone()));
-    run.part(|| part2(input.clone()));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
-#[ignore]
 fn example() {
     let input = parse_input("389125467");
     assert_eq!(part1(input.clone()), "67384529");
@@ -74,7 +96,6 @@ fn example() {
 }
 
 #[test]
-#[ignore]
 fn default() {
     let input = default_input();
     assert_eq!(part1(input.clone()), "49725386");
