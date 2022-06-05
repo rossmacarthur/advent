97fa4f9d4163734d79ef1dcd921d9850b001d649
Ross MacArthur <ross@macarthur.io>
2022-06-05 22:31:08 +0200

    advent: Refactor summary rendering

--- crates/advent/src/lib.rs
+++ crates/advent/src/lib.rs
@@ -1,64 +1,23 @@
 mod human;
+mod output;
 pub mod prelude;
 mod stats;
+mod types;
 
 use std::fmt::Display;
+use std::io;
 use std::time::{Duration, Instant};
 
 use argh::FromArgs;
 use peter::Stylize;
+
+use crate::types::{Bench, Run, Summary};
 
 type FnBox<'a> = Box<dyn Fn() -> Box<dyn Display + 'a> + 'a>;
 
 #[derive(Default)]
 pub struct Advent<'a> {
     parts: Vec<(Option<String>, FnBox<'a>)>,
-}
-
-fn print_bench_summary(i: usize, name: Option<String>, stats: stats::Stats) {
-    if i != 0 {
-        println!();
-    }
-    let name = name.unwrap_or_else(|| format!("Part {}", i + 1));
-    println!(
-        "{}{:>width$}",
-        name.bold(),
-        human::Samples::new(stats.len).fixed(245),
-        width = 46 - name.chars().count(),
-    );
-    let mean = human::Time::new(stats.mean);
-    let std_dev = human::Time::with_scale(stats.std_dev, mean.scale());
-    let min = human::Time::with_scale(stats.min, mean.scale());
-    let max = human::Time::with_scale(stats.max, mean.scale());
-    println!(
-        "  Time ({} ± {}):       {:>9} ± {:>9}",
-        "mean".green().bold(),
-        "σ".green(),
-        mean.green().bold(),
-        std_dev.green(),
-    );
-    println!(
-        "  Range ({} … {}):     {:>9} … {:>9}",
-        "min".cyan(),
-        "max".magenta(),
-        min.cyan(),
-        max.magenta(),
-    );
-}
-
-fn print_run_summary(i: usize, name: Option<String>, result: String, elapsed: human::Time) {
-    if i != 0 {
-        println!();
-    }
-    let name = name.unwrap_or_else(|| format!("Part {}", i + 1));
-    let width = 46_usize.saturating_sub(name.chars().count() + 2);
-    println!(
-        "{}: {:>width$}\n{}",
-        name.bold().cyan(),
-        format!("({})", elapsed).fixed(245),
-        result.bold(),
-        width = width,
-    )
 }
 
 impl<'a> Advent<'a> {
@@ -79,16 +38,26 @@ impl<'a> Advent<'a> {
         self.parts.push((name, Box::new(move || Box::new(f()))))
     }
 
-    fn once(self) {
+    fn run(self) -> Summary {
+        let mut runs = Vec::new();
+
         for (i, (name, f)) in self.parts.into_iter().enumerate() {
             let start = Instant::now();
             let result = f();
             let elapsed = (Instant::now() - start).as_secs_f64();
-            print_run_summary(i, name, result.to_string(), human::Time::new(elapsed));
+            runs.push(Run {
+                name: name.unwrap_or_else(|| format!("Part {}", i + 1)),
+                result: result.to_string(),
+                elapsed,
+            })
         }
+
+        Summary::Run(runs)
     }
 
-    pub fn bench(self) {
+    fn bench(self) -> Summary {
+        let mut benches = Vec::new();
+
         for (i, (name, f)) in self.parts.into_iter().enumerate() {
             const FIVE_SECS: Duration = Duration::from_secs(5);
             const THREE_SECS: Duration = Duration::from_secs(3);
@@ -110,8 +79,13 @@ impl<'a> Advent<'a> {
                 times.push((t1 - t0).as_secs_f64());
             }
 
-            print_bench_summary(i, name, stats::basics(times));
+            benches.push(Bench {
+                name: name.unwrap_or_else(|| format!("Part {}", i + 1)),
+                stats: stats::basics(times),
+            });
         }
+
+        Summary::Bench(benches)
     }
 
     pub fn finish(self) {
@@ -130,7 +104,7 @@ impl<'a> Advent<'a> {
         if !no_trees {
             println!("{}", ascii_art::fun());
         }
-        if bench {
+        let summary = if bench {
             if cfg!(not(profile = "release")) {
                 eprintln!(
                     "{}\n",
@@ -139,8 +113,10 @@ impl<'a> Advent<'a> {
             }
             self.bench()
         } else {
-            self.once()
-        }
+            self.run()
+        };
+
+        output::print_summary(io::stdout(), &summary).unwrap();
     }
 }
 
