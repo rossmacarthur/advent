8e11efd83c8def8fd3cc25d792aa4b156d5482c5
Ross MacArthur <ross@macarthur.io>
2022-06-05 17:57:26 +0200

    Use prelude in 2021 Day 6 solution

--- 2021/06.rs
+++ 2021/06.rs
@@ -11,10 +11,10 @@ fn default_input() -> Vec<usize> {
     parse_input(include_str!("input/06.txt"))
 }
 
-fn solve(fish: &[usize], days: usize) -> usize {
+fn solve(fish: Vec<usize>, days: usize) -> usize {
     let mut counts = [0; 9];
     for age in fish {
-        counts[*age] += 1;
+        counts[age] += 1;
     }
     for _ in 0..days {
         counts.rotate_left(1);
@@ -23,32 +23,31 @@ fn solve(fish: &[usize], days: usize) -> usize {
     counts.iter().sum()
 }
 
-fn part1(fish: &[usize]) -> usize {
+fn part1(fish: Vec<usize>) -> usize {
     solve(fish, 80)
 }
 
-fn part2(fish: &[usize]) -> usize {
+fn part2(fish: Vec<usize>) -> usize {
     solve(fish, 256)
 }
 
 fn main() {
-    let input = default_input();
     let mut run = advent::start();
-    run.part(|| part1(&input));
-    run.part(|| part2(&input));
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
     run.finish();
 }
 
 #[test]
 fn example() {
     let input = parse_input("3,4,3,1,2");
-    assert_eq!(solve(&input, 18), 26);
-    assert_eq!(part2(&input), 26984457539);
+    assert_eq!(solve(input.clone(), 18), 26);
+    assert_eq!(part2(input), 26984457539);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    assert_eq!(part1(&input), 360610);
-    assert_eq!(part2(&input), 1631629590423);
+    assert_eq!(part1(input.clone()), 360610);
+    assert_eq!(part2(input), 1631629590423);
 }
