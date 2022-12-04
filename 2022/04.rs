35d4a2460920966a89d58ddeacdce71449bc525f
Ross MacArthur <ross@macarthur.io>
2022-12-04 07:43:05 +0200

    Add 2022 Day 4 solution

---
+++ 2022/04.rs
@@ -0,0 +1,61 @@
+use advent::prelude::*;
+
+fn parse_input(input: &str) -> Vec<[i64; 4]> {
+    input
+        .lines()
+        .map(|line| {
+            line.split(',')
+                .flat_map(|s| s.split('-'))
+                .map(str::parse)
+                .map(Result::unwrap)
+                .next_array()
+                .unwrap()
+        })
+        .collect()
+}
+
+fn default_input() -> Vec<[i64; 4]> {
+    parse_input(include_str!("input/04.txt"))
+}
+
+fn part1(pairs: Vec<[i64; 4]>) -> usize {
+    pairs
+        .into_iter()
+        .filter(|&[a, b, m, n]| (a >= m && b <= n) || (m >= a && n <= b))
+        .count()
+}
+
+fn part2(pairs: Vec<[i64; 4]>) -> usize {
+    pairs
+        .into_iter()
+        .filter(|&[a, b, m, n]| (a <= m && b >= m) || (m <= a && n >= a))
+        .count()
+}
+
+fn main() {
+    let mut run = advent::start();
+    run.part(|| part1(default_input()));
+    run.part(|| part2(default_input()));
+    run.finish();
+}
+
+#[test]
+fn example() {
+    let input = parse_input(
+        "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8",
+    );
+    assert_eq!(part1(input.clone()), 2);
+    assert_eq!(part2(input), 4);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(input.clone()), 576);
+    assert_eq!(part2(input), 905);
+}
