a37c79039a7d3a915b76ef4fcd45ba5a94daf816
Ross MacArthur <ross@macarthur.io>
2022-12-20 06:57:59 +0200

    viz

--- 2022/14.rs
+++ 2022/14.rs
@@ -27,13 +27,77 @@ fn default_input() -> HashSet<Vector2> {
 }
 
 fn solve(mut cave: HashSet<Vector2>, part2: bool) -> usize {
+    use image::{ImageBuffer, Rgb};
+
+    let prefix = if part2 { "part2" } else { "part1" };
+
+    let min_x = cave.iter().map(|v| v.x).min().unwrap();
+    let min_y = cave.iter().map(|v| v.y).min().unwrap();
     let mut max_y = cave.iter().map(|v| v.y).max().unwrap();
     if part2 {
         max_y += 2;
     }
+
+    let shift_x: i64 = 40;
+    let shift_y: i64 = 3;
+
+    // the padding around the image
+    let pad = 20;
+    // the size of each sand block
+    let block = 5;
+
+    let height = (max_y - min_y) as u32;
+    let mut img = ImageBuffer::new(
+        block * height + (2 * shift_y as u32) + 2 * pad,
+        block * height + (2 * shift_y as u32) + 2 * pad,
+    );
+
+    // Draw a gray background
+    for pixel in img.pixels_mut() {
+        *pixel = Rgb([0x1f, 0x1f, 0x1f]);
+    }
+    let draw_point = |img: &mut ImageBuffer<_, _>, p: Vector2, color: Rgb<u8>| {
+        for dx in 0..block {
+            for dy in 0..block {
+                let x = block * (p.x - min_x + shift_x) as u32 + dx;
+                let y = block * (p.y - min_y + shift_y) as u32 + dy;
+                if let Some(pixel) = img.get_pixel_mut_checked(x, y) {
+                    *pixel = color;
+                } else {
+                    // eprintln!("{p:?} out of bounds -> {x},{y}");
+                }
+            }
+        }
+    };
+
+    // Draw the walls
+    for p in &cave {
+        draw_point(&mut img, *p, Rgb([0x7f, 0x7f, 0x7f]));
+    }
+
     let mut flowing = vec![vector![500, 0]];
     let mut count = 0;
+    let mut id = 0;
+
+    let dir = std::path::PathBuf::from_iter([env!("CARGO_WORKSPACE_DIR"), "target", "visual"]);
+    std::fs::create_dir_all(&dir).unwrap();
+
     while let Some(sand) = flowing.last().copied() {
+        if (count < 1000 && count % 10 == 0)
+            || (count < 2000 && count % 20 == 0)
+            || (count >= 2000 && count % 40 == 0)
+        {
+            let mut img2 = img.clone();
+            for f in &flowing {
+                draw_point(&mut img2, *f, Rgb([0xb2, 0x70, 0x70]));
+            }
+            let p = dir.join(format!("{prefix}-{id:04}.png"));
+            println!("{}", p.display());
+            img2.save(p).unwrap();
+            id += 1;
+            // break;
+        }
+
         // Check if there is air in any of the following directions
         match vectors![[0, 1], [-1, 1], [1, 1]]
             .into_iter()
@@ -48,6 +112,7 @@ fn solve(mut cave: HashSet<Vector2>, part2: bool) -> usize {
             // Otherwise we can no longer flow, so we just insert sand at this
             // location
             _ => {
+                draw_point(&mut img, sand, Rgb([0xb2, 0xa2, 0x70]));
                 cave.insert(sand);
                 flowing.pop();
                 count += 1;
