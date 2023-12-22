393e4fa7e55003f270ac8cd0611881520d182ea3
Ross MacArthur <ross@macarthur.io>
2023-12-22 07:00:17 +0200

    cli: Only download puzzle input if it's released

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -111,13 +111,17 @@ fn new(year: u32, day: u32) -> Result<()> {
     const TEMPLATE: &str = include_str!("template.rs");
     let bin_display = bin.strip_prefix(&workspace_dir).unwrap_or(&bin).display();
     if bin.exists() {
-        println!("• {bin_display} already exists");
+        print("Checked", format!("binary source `{bin_display}`"));
     } else {
         let rendered = TEMPLATE
             .replace("{ year }", &format!("{year:04}"))
             .replace("{ day }", &format!("{day:02}"));
+        fs::create_dir_all(bin.parent().unwrap())?;
         fs::write(&bin, rendered)?;
-        println!("• {bin_display} was created");
+        print(
+            "Created",
+            format!("binary source `{bin_display}` from template"),
+        );
     }
 
     // Update Cargo.toml
@@ -135,13 +139,20 @@ fn new(year: u32, day: u32) -> Result<()> {
     bins.bin.dedup();
     let binaries = toml::to_string(&bins)?;
     fs::write(&manifest_path, main.to_owned() + &binaries)?;
+
     if added {
-        println!("• {bin_name} binary added to Cargo manifest");
+        print(
+            "Updated",
+            format!("Cargo manifest with `{bin_name}` binary"),
+        );
     } else {
-        println!("• {bin_name} binary already exists in Cargo manifest");
+        print(
+            "Checked",
+            format!("Cargo manifest contains `{bin_name}` binary"),
+        );
     }
 
-    println!("All done! Use `cargo advent -y {year} -d {day} run` to run");
+    check_input(year, day)?;
 
     Ok(())
 }
@@ -153,15 +164,22 @@ fn open(year: u32, day: u32, args: &[String]) -> Result<()> {
     Ok(())
 }
 
-fn print(header: &str, message: impl Display) {
-    if io::stdout().is_terminal() {
-        println!("{:>12} {}", Paint::new(&header).bold().green(), message);
-    } else {
-        println!("{:>12} {}", header, message);
-    }
-}
-
 fn check_input(year: u32, day: u32) -> Result<()> {
+    // Only try download if the puzzle has actually been released
+    let now = time::OffsetDateTime::now_utc();
+    let puzzle = time::PrimitiveDateTime::new(
+        time::Date::from_calendar_date(year as i32, time::Month::December, day as u8)?,
+        time::Time::from_hms(5, 0, 0)?,
+    )
+    .assume_utc();
+    if now < puzzle {
+        warning(
+            "Unavailable",
+            format!("puzzle input (year: {year:04}, day: {day:02})"),
+        );
+        return Ok(());
+    }
+
     // Calculate input file path
     let workspace_dir = PathBuf::from(env!("CARGO_WORKSPACE_DIR"));
     let input = workspace_dir.join(format!("input/{year:04}/{day:02}.txt"));
@@ -175,16 +193,13 @@ fn check_input(year: u32, day: u32) -> Result<()> {
         .display();
 
     if !input.exists() {
-        print(
-            "Downloading",
-            format!("puzzle input (year: {year}, day: {day:02})"),
-        );
         let url = format!("https://adventofcode.com/{year}/day/{day}/input");
         let text = download(&url)?;
         fs::write(&input, text)?;
+        print("Downloaded", format!("puzzle input `{input_display}`"));
+    } else {
+        print("Verified", format!("puzzle input `{input_display}`"));
     }
-
-    print("Verified", format!("puzzle input `{input_display}`"));
 
     Ok(())
 }
@@ -220,4 +235,20 @@ fn cargo(cmd: String, year: u32, day: u32, args: &[String]) -> Result<()> {
         .status()?;
 
     process::exit(status.code().unwrap())
+}
+
+fn print(header: &str, message: impl Display) {
+    if io::stdout().is_terminal() {
+        println!("{:>12} {}", Paint::new(&header).bold().green(), message);
+    } else {
+        println!("{:>12} {}", header, message);
+    }
+}
+
+fn warning(header: &str, message: impl Display) {
+    if io::stdout().is_terminal() {
+        println!("{:>12} {}", Paint::new(&header).bold().yellow(), message);
+    } else {
+        println!("{:>12} {}", header, message);
+    }
 }
